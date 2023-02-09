9a2ecb66802792e0684264ac01584a54fb3a70c5
Adrien Bocquet <adrienbocquet38@gmail.com>
2023-02-09 20:54:54 +0100

    Add QTensor class (#4)
    * Add utils
    
    * Add QTensor class
    
    * Renaming and error handling
    
    Changed the file name to `qtensor.py` (stylistic preference). 
    Renamed `self.to_qutip()` to `self.qutip()` to be consistent with torch naming self.numpy().
    More details in error message.
    
    * Update __init__.py
    
    ---------
    
    Co-authored-by: PierreGuilmin <pierreguilmin@gmail.com>
    Co-authored-by: Ronan Gautier <ron.gautier@gmail.com>

--- torchq/__init__.py
+++ torchq/__init__.py
@@ -1,4 +1,6 @@
 from .utils import *
+from .qtensor import QTensor
 from .mesolve import mesolve
 from .ssolve import ssolve
 from .smesolve import smesolve
+
---
+++ torchq/qtensor.py
@@ -0,0 +1,61 @@
+from typing import List
+
+import numpy as np
+import qutip as qt
+import torch
+
+
+class QTensor(torch.Tensor):
+    """
+    A torch tensor that represents a quantum object (operator, density matrix, state vector...)
+
+    Subclasses of Tensor can slow torch down significantly
+    TODO: check performance impact and implement mitigations
+    Ref : https://benjaminwarner.dev/2022/06/14/debugging-pytorch-performance-decrease
+    """
+
+    def __init__(self, *args, **kwargs):
+        """ Creates a QTensor
+            Args:
+                - arguments inherited from torch.Tensor
+                - dims: the dimension of the Hilbert sub-spaces
+         """
+        dims = kwargs.pop("dims", None)
+        super().__init__(*args, **kwargs)
+
+        if dims is None:
+            raise ValueError("Argument 'dims' must be specified at QTensor initialisation.")
+        self.dims = dims
+
+    def qutip(self) -> qt.Qobj:
+        """Convert a PyTorch tensor to a QuTiP quantum object.
+
+        Note:
+            The returned object does not share memory with the argument, it is
+            simply a copy.
+        Returns:
+            QuTiP quantum object.
+        """
+        return qt.Qobj(self.numpy(force=True), dims=self.dims)
+
+    @staticmethod
+    def from_qutip(x: qt.Qobj) -> torch.tensor:
+        """Convert a QuTiP quantum object to a PyTorch tensor.
+
+        Note:
+            The returned object does not share memory with the argument, it is
+            simply a copy.
+
+        Args:
+            x: QuTiP quantum object.
+
+        Returns:
+            TorchQ QTensor.
+        """
+        return QTensor(torch.from_numpy(x.full()), dims=x.dims)
+
+    @staticmethod
+    def from_numpy(x: np.array, dims: List[int]):
+        return QTensor(torch.from_numpy(x.full()), dims=dims)
+
+
