9172bc749db31596828422f1fde03ace6a57ba2c
Ronan Gautier <ron.gautier@gmail.com>
2023-02-15 10:43:28 -0500

    Correct wrong import

--- torchqdynamics/ode/adjoint.py
+++ torchqdynamics/ode/adjoint.py
@@ -6,6 +6,7 @@ import torchqdynamics.ode as ode
 #     Adjoint integrator base class
 # -------------------------------------------------------------------------------------------------
 
+
 class ODEIntAdjoint(torch.autograd.Function):
     @staticmethod
     def forward(ctx, f, f_adj, y0, tspan, solver, save_at, atol, rtol, *params):
@@ -16,10 +17,10 @@ class ODEIntAdjoint(torch.autograd.Function):
         ctx.solver = solver
         ctx.atol = atol
         ctx.rtol = rtol
-        
+
         # Solve the ODE forward, save results and save model parameters
         with torch.no_grad():
-            t, y = grt.odeint(f, y0, tspan, solver, save_at, atol=atol, rtol=rtol)
+            t, y = ode.odeint(f, y0, tspan, solver, save_at, atol=atol, rtol=rtol)
             ctx.save_for_backward(t, y, *params)
         return t, y
 
@@ -27,7 +28,7 @@ class ODEIntAdjoint(torch.autograd.Function):
     def backward(ctx, *grad_y):
         # Disable gradient computation
         torch.set_grad_enabled(False)
-        
+
         # Unpack context
         f = ctx.f
         f_adj = ctx.f_adj
@@ -42,7 +43,7 @@ class ODEIntAdjoint(torch.autograd.Function):
         params_shapes = tuple(p.shape for p in params)
         params_numels = torch.tensor([p.numel() for p in params])
         params_indices = torch.cat([torch.tensor([0]), torch.cumsum(params_numels, dim=0)])
-        
+
         # Initialize the augmented state as a flat tensor
         # y is the state, a is the adjoint, g is the gradient w.r.t. parameters
         yT, aT, gT = y_saved[-1], grad_y[-1][-1], torch.zeros(n_params, dtype=y_saved.dtype)
@@ -55,66 +56,79 @@ class ODEIntAdjoint(torch.autograd.Function):
             # Define the augmented function for RK solvers
             def aug_f(t, aug_y):
                 # Unpack and reshape state
-                y, a = aug_y[:y_len], aug_y[y_len:y_len+a_len]
+                y, a = aug_y[:y_len], aug_y[y_len : y_len + a_len]
                 y, a = y.view(y_shape), a.view(a_shape)
-                # Compute the augmented state 
+                # Compute the augmented state
                 with torch.enable_grad():
                     y, a = y.requires_grad_(True), a.requires_grad_(True)
                     dy, da = f(t, y), f_adj(t, a)
                     dy, da = dy.view(-1), da.view(-1)
                     # Compute dg/dt = - (d(da/dt)/dtheta @ y)
-                    dg = torch.autograd.grad(da, params, y.view(-1), 
-                                            allow_unused=True, retain_graph=True)
+                    dg = torch.autograd.grad(
+                        da, params, y.view(-1), allow_unused=True, retain_graph=True
+                    )
                     # Convert back dg to flat tensor
-                    dg = torch.cat([torch.zeros_like(p).view(-1) if dg_ is None else dg_.view(-1)
-                                for p, dg_ in zip(params, dg)]).conj()
+                    dg = torch.cat(
+                        [
+                            torch.zeros_like(p).view(-1) if dg_ is None else dg_.view(-1)
+                            for p, dg_ in zip(params, dg)
+                        ]
+                    ).conj()
 
                 # Repack and return
                 return torch.cat([dy, da, dg])
+
         elif solver_type == "out":
             # Define the augmented function for outsourced solvers
             def aug_f(t, dt, aug_y):
                 # Unpack and reshape state
-                y, a, g = aug_y[:y_len], aug_y[y_len:y_len+a_len], aug_y[-g_len:]
+                y, a, g = aug_y[:y_len], aug_y[y_len : y_len + a_len], aug_y[-g_len:]
                 y, a, g = y.view(y_shape), a.view(a_shape), g.view(g_shape)
 
-                # Compute the augmented state 
+                # Compute the augmented state
                 with torch.enable_grad():
                     y, a = y.requires_grad_(True), a.requires_grad_(True)
                     dy, da = f(t, dt, y), f_adj(t, dt, a)
                     dy, da = dy.view(-1), da.view(-1)
                     # Compute dg = - (d(da)/dtheta @ y)
-                    dg = torch.autograd.grad(da, params, y.view(-1),
-                                             allow_unused=True, retain_graph=True)
+                    dg = torch.autograd.grad(
+                        da, params, y.view(-1), allow_unused=True, retain_graph=True
+                    )
                     # Convert back dg to flat tensor
-                    dg = torch.cat([torch.zeros_like(p).view(-1) if dg_ is None else dg_.view(-1)
-                                for p, dg_ in zip(params, dg)]).conj()
+                    dg = torch.cat(
+                        [
+                            torch.zeros_like(p).view(-1) if dg_ is None else dg_.view(-1)
+                            for p, dg_ in zip(params, dg)
+                        ]
+                    ).conj()
                     # Add previous value
-                    dg = g.view(-1) + dg 
+                    dg = g.view(-1) + dg
 
                 # Repack and return
                 return torch.cat([dy, da, dg])
-            
+
         # Solve the augmented equation backward between every checkpoints
         for i in range(len(t_saved) - 1, 0, -1):
             # Get tspan between checkpoints
-            t0, t1 = t_saved[i-1], t_saved[i]
+            t0, t1 = t_saved[i - 1], t_saved[i]
             tspan_ = tspan[torch.logical_and(tspan > t0, tspan < t1)]
             tspan_ = torch.cat([torch.tensor([t0]), tspan_, torch.tensor([t1])])
             # Compute the augmented ODE
-            _, aug_y = grt.odeint(aug_f, aug_y, tspan_, solver=solver, save_at=(),
-                              atol=atol, rtol=rtol, backward_mode=True)
+            _, aug_y = ode.odeint(aug_f, aug_y, tspan_, solver=solver, save_at=(), 
+                                  atol=atol, rtol=rtol, backward_mode=True)
             aug_y = aug_y[-1]
             # Replace y with its checkpointed version
-            aug_y[:y_len] = y_saved[i-1].view(-1)
+            aug_y[:y_len] = y_saved[i - 1].view(-1)
             # Update adjoint wrt this time point by adding dL / dy(t)
-            aug_y[y_len:y_len + a_len] += grad_y[-1][i-1].view(-1)
+            aug_y[y_len : y_len + a_len] += grad_y[-1][i - 1].view(-1)
 
         # Extract the relevant gradients to return
-        a, g = aug_y[y_len:y_len + a_len], aug_y[-g_len:]
-        
+        a, g = aug_y[y_len : y_len + a_len], aug_y[-g_len:]
+
         # Reshape outputs
         adjoint_out = a.reshape(a_shape)
-        grads_out = tuple(g[params_indices[i]:params_indices[i+1]].reshape(params_shapes[i])
-                          for i in range(len(params)))
+        grads_out = tuple(
+            g[params_indices[i] : params_indices[i + 1]].reshape(params_shapes[i])
+            for i in range(len(params))
+        )
         return (None, None, adjoint_out, None, None, None, None, None, *grads_out)
