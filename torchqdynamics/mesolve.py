4a90b0e6be496acb7a8f91810ec49748180fdbec
Ronan Gautier <ron.gautier@gmail.com>
2023-02-27 22:35:05 -0500

    mesolve backbone functions

--- torchqdynamics/mesolve.py
+++ torchqdynamics/mesolve.py
@@ -1,63 +1,101 @@
-from typing import Any, Dict, List, Optional
+from dataclasses import dataclass
 
-import numpy as np
 import torch
-import torchdiffeq
+
+from .odeint import odeint
+
+# --------------------------------------------------------------------------------------
+#     Main mesolve function
+# --------------------------------------------------------------------------------------
 
 
 def mesolve(
-    H: torch.Tensor,
-    Ls: List[torch.Tensor],
-    rho0: torch.Tensor,
-    tlist: np.ndarray,
-    solver: str = 'rk4',
-    options: Optional[Dict[str, Any]] = None,
-) -> torch.Tensor:
-    if options is None:
-        options = {}
+    H, jump_ops, rho0, tsave, solver, sensitivity='autograd', model_params=None
+):
+    # Define the QSolver
+    if isinstance(solver, Rouchon):
+        if solver.order == 1:
+            qsolver = MERouchon1(H, jump_ops, solver)
+        elif solver.order == 2:
+            qsolver = MERouchon2(H, jump_ops, solver)
+    else:
+        raise NotImplementedError
 
-    if solver == 'rk4':
-        if not 'step_size' in options:
-            options['step_size'] = 0.001
-        return _mesolve_torchdiffeq(H, Ls, rho0, tlist, options)
-
-    raise ValueError('invalid solver (supported: \'rk4\')')
-
-
-def _dissipator(L: torch.tensor, Ldag: torch.tensor, rho: torch.tensor) -> torch.tensor:
-    # [speedup] by using the precomputed jump operator adjoint
-    return L @ rho @ Ldag - 0.5 * Ldag @ L @ rho - 0.5 * rho @ Ldag @ L
+    # Compute the result
+    return odeint(
+        qsolver, rho0, tsave, sensitivity=sensitivity, model_params=model_params
+    )
 
 
-def _lindbladian(
-    H: torch.Tensor,
-    Ls: torch.Tensor,
-    Lsdag: torch.Tensor,
-    rho: torch.Tensor,
-) -> torch.Tensor:
-    # [speedup] by using the precomputed jump operators adjoint
-    return -1j * (H @ rho - rho @ H) + _dissipator(Ls, Lsdag, rho).sum(0)
+# --------------------------------------------------------------------------------------
+#     ME QSolver classes
+# --------------------------------------------------------------------------------------
 
 
-def _mesolve_torchdiffeq(
-    H: torch.Tensor,
-    Ls: List[torch.Tensor],
-    rho0: torch.Tensor,
-    tlist: np.ndarray,
-    options: Dict[str, Any],
-) -> torch.Tensor:
-    Ls = torch.stack(Ls)
-    # [speedup] by precomputing the jump operators adjoint
-    Lsdag = Ls.adjoint().resolve_conj()
+class QSolver:
+    def __init__(self):
+        pass
 
-    def f(t, x):
-        rho = torch.view_as_complex(x)
-        drho = _lindbladian(H, Ls, Lsdag, rho)
-        return torch.view_as_real(drho)
 
-    y0 = torch.view_as_real(rho0)
-    t = torch.from_numpy(tlist)
-    options = {'step_size': options['step_size']}
+class MERouchon(QSolver):
+    def __init__(self, H, jump_ops, solver_options):
+        self.H = H
+        self.jump_ops = jump_ops
+        self.jumpdag_ops = jump_ops.adjoint()
+        self.sum_nojump = (self.jumpdag_ops @ self.jump_ops).sum(dim=0)
+        self.I = torch.eye(H(0).shape[-1]).to(H(0))
+        self.options = solver_options
 
-    ys = torchdiffeq.odeint(f, y0, t, method='rk4', options=options)
-    return torch.view_as_complex(ys)
+
+class MERouchon1(MERouchon):
+    def forward(self, t, dt, rho):
+        """Compute rho(t+dt) using a Rouchon method of order 1"""
+        # Non-hermitian Hamiltonian at time t
+        H_nh = self.H(t) - 0.5j * self.sum_nojump
+
+        # Build time-dependent Kraus operators
+        M0 = self.I - 1j * dt * H_nh
+        M1s = self.jump_ops
+
+        # Compute rho(t+dt)
+        drho = M0 @ rho @ M0.adjoint(
+        ) + dt * (self.jump_ops @ rho.unsqueeze(0) @ self.jumpdag_ops).sum(dim=0)
+
+        return drho
+
+    def forward_adjoint(self, t, dt, phi):
+        raise NotImplementedError
+
+
+class MERouchon2(MERouchon):
+    def forward(self, t, dt, rho):
+        """Compute rho(t+dt) using a Rouchon method of order 2"""
+        # Non-hermitian Hamiltonian at time t
+        H_nh = self.H(t) - 0.5j * self.sum_nojump
+
+        # Build time-dependent Kraus operators
+        M0 = self.I - 1j * dt * H_nh - 0.5 * dt**2 * H_nh**2
+        M1s = 0.5 * (self.Ls @ M0 + M0 @ self.Ls)
+
+        # Compute rho(t+dt)
+        drho_ = dt * torch.sum(M1s @ rho.unsqueeze(0) @ M1s.adjoint(), dim=0)
+        drho += 0.5 * dt * torch.sum(M1s @ drho_.unsqueeze(0) @ M1s.adjoint(), dim=0)
+        drho += drho_ + M0 @ rho @ M0.adjoint()
+
+        return drho
+
+    def forward_adjoint(self, t, dt, phi):
+        raise NotImplementedError
+
+
+# --------------------------------------------------------------------------------------
+#     ME Solver Options
+# --------------------------------------------------------------------------------------
+
+# See the PR by @abocquet at https://github.com/PierreGuilmin/torchqdynamics/pull/10
+
+
+@dataclass
+class Rouchon:
+    dt: float
+    stepclass: str = 'fixed'
---
+++ torchqdynamics/odeint.py
@@ -0,0 +1,95 @@
+import warnings
+
+import torch
+
+# --------------------------------------------------------------------------------------
+#     Main odeint function
+# --------------------------------------------------------------------------------------
+
+
+def odeint(qsolver, y0, tsave, sensitivity='autograd', model_params=None):
+    # Check arguments
+    init_tsave(tsave)
+    if (model_params is not None) and (sensitivity in [None, 'autograd']):
+        warnings.warn('Argument `model_params` was supplied in `odeint` but not used.')
+
+    # Dispatch to appropriate odeint subroutine
+    if sensitivity is None:
+        return odeint_inplace(qsolver, y0, tsave)
+    elif sensitivity == 'autograd':
+        return odeint_main(qsolver, y0, tsave)
+    elif sensitivity == 'adjoint':
+        return odeint_adjoint(qsolver, y0, tsave, model_params)
+
+
+# --------------------------------------------------------------------------------------
+#     Core subroutines
+# --------------------------------------------------------------------------------------
+
+
+def odeint_main(qsolver, y0, tsave):
+    if qsolver.options.stepclass == 'fixed':
+        return _fixed_odeint(qsolver, y0, tsave)
+    elif qsolver.options.stepclass == 'adaptive':
+        return _adaptive_odeint(qsolver, y0, tsave)
+
+
+def odeint_inplace(qsolver, y0, tsave):
+    raise NotImplementedError
+
+
+def odeint_adjoint(qsolver, y0, tsave, model_params):
+    raise NotImplementedError
+
+
+def _adaptive_odeint(qsolver, y0, tsave):
+    raise NotImplementedError
+
+
+def _fixed_odeint(qsolver, y0, tsave):
+    # Initialize save tensor
+    ysave = torch.zeros((len(tsave), ) + y0.shape).to(y0)
+    save_counter, save_flag = 0, False
+    if tsave[0] == 0:
+        ysave[0] = y0
+        save_counter += 1
+
+    # Get qsolver fixed time step
+    dt = qsolver.options.dt
+
+    # Run the ODE routine
+    t, y = 0, y0
+    while t < tsave[-1]:
+        # Check if final time is reached
+        if t + dt > tsave[-1]:
+            dt = tsave[-1] - t
+
+        # Iterate solution
+        y = qsolver.forward(t, dt, y)
+        t = t + dt
+
+        # Save solution
+        if t >= tsave[save_counter]:
+            ysave[save_counter] = y
+            save_counter += 1
+
+    return tsave, ysave
+
+
+# --------------------------------------------------------------------------------------
+#     Utility functions
+# --------------------------------------------------------------------------------------
+
+
+def init_tsave(tsave):
+    """Check tsave is a sorted 1-D torch.tensor"""
+    if isinstance(tsave, (list, np.ndarray)):
+        tsave = torch.cat(tsave)
+    if tsave.dim != 1 or len(tsave) == 0:
+        raise ValueError('Argument `tsave` should be a non-empty 1-D torch.Tensor.')
+    if not torch.all(torch.diff(tsave) > 0):
+        raise ValueError(
+            'Argument `tsave` is not sorted in ascending order '
+            'or contains duplicate values.'
+        )
+    return tsave
