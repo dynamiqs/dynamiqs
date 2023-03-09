a86453ab3c7623064f6845741046bdbbb3d3daeb
Ronan Gautier <ron.gautier@gmail.com>
2023-03-09 11:23:02 -0800

    Add ME Rouchon order 1.5 and 2

--- torchqdynamics/mesolve.py
+++ torchqdynamics/mesolve.py
@@ -16,6 +16,8 @@ def mesolve(
     if isinstance(solver, Rouchon):
         if solver.order == 1:
             qsolver = MERouchon1(H, jump_ops, solver)
+        elif solver.order == 1.5:
+            qsolver = MERouchon1_5(H, jump_ops, solver)
         elif solver.order == 2:
             qsolver = MERouchon2(H, jump_ops, solver)
     else:
@@ -49,7 +51,7 @@ class MERouchon(QSolver):
 class MERouchon1(MERouchon):
     def forward(self, t, dt, rho):
         """Compute rho(t+dt) using a Rouchon method of order 1."""
-        # mon-hermitian Hamiltonian at time t
+        # non-hermitian Hamiltonian at time t
         H_nh = self.H(t) - 0.5j * self.sum_nojump
 
         # build time-dependent Kraus operator
@@ -67,9 +69,61 @@ class MERouchon1(MERouchon):
         raise NotImplementedError
 
 
+class MERouchon1_5(MERouchon):
+    def forward(self, t, dt, rho):
+        """Compute rho(t+dt) using a Rouchon method of order 1.5."""
+        # non-hermitian Hamiltonian at time t
+        H_nh = self.H(t) - 0.5j * self.sum_nojump
+
+        # build time-dependent Kraus operators
+        M0 = self.I - 1j * dt * H_nh
+        S = M0.adjoint() @ M0 + dt * self.sum_nojump
+        S_inv_sqrtm = self.inv_sqrtm(S)
+
+        # compute rho(t+dt)
+        rho = S_inv_sqrtm @ rho @ S_inv_sqrtm.adjoint()
+        rho = (
+            M0 @ rho @ M0.adjoint() + dt *
+            (self.jump_ops @ rho.unsqueeze(0) @ self.jumpdag_ops).sum(dim=0)
+        )
+        return drho
+
+    def forward_adjoint(self, t, dt, phi):
+        raise NotImplementedError
+
+    def inv_sqrtm(self, A):
+        """Compute the inverse square root of a matrix using its eigendecomposition.
+
+        TODO: Replace with Schur decomposition once released by PyTorch.
+        See the feature request at https://github.com/pytorch/pytorch/issues/78809.
+        """
+        vals, vecs = torch.linalg.eigh(A)
+        return vecs @ torch.linalg.solve(vecs, torch.diag(vals**(-0.5)), left=False)
+
+
 class MERouchon2(MERouchon):
     def forward(self, t, dt, rho):
-        raise NotImplementedError
+        """Compute rho(t+dt) using a Rouchon method of order 2.
+
+        NB: For fast time-varying Hamiltonians, this method is not order 2 because the
+        second-order time derivative term is neglected. This term should be added in the
+        zero-th order Kraus operator, as M0 += -0.5j * dt**2 * \dot{H}.
+        """
+        # non-hermitian Hamiltonian at time t
+        H_nh = self.H(t) - 0.5j * self.sum_nojump
+
+        # build time-dependent Kraus operators
+        M0 = self.I - 1j * dt * H_nh - 0.5 * dt**2 * H_nh @ H_nh
+        M1s = 0.5 * (self.jump_ops @ M0 + M0 @ self.jump_ops)
+
+        # compute rho(t+dt)
+        rho_ = dt * (M1s @ rho.unsqueeze(0) @ M1s.adjoint()).sum(dim=0)
+        rho = (
+            M0 @ rho @ M0.adjoint() + drho_ + 0.5 * dt *
+            (M1s @ rho_.unsqueeze(0) @ M1s.adjoint()).sum(dim=0)
+        )
+        rho = rho / rho.trace()
+        return drho
 
     def forward_adjoint(self, t, dt, phi):
         raise NotImplementedError
