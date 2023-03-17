786c9543d2c423cb9474253cf83dee511ac30833
Ronan Gautier <ron.gautier@gmail.com>
2023-03-17 10:18:11 -0700

    Add ME Rouchon of order 1.5 and 2 (#13)
    * Add ME Rouchon order 1.5 and 2
    
    * Small corrections
    
    * Add comment on sqrtm implementation
    
    * Debug
    
    * Default solver to None
    
    * Add support for exp_ops (#15)
    
    * Support different t_save and t_step + exp_ops
    
    * Pre-allocate exp_ops and use abstract method decorator
    
    * Remove t_step for dt
    
    * Address @PierreGuilmin comments
    
    * Go back to check_t_save
    
    * Implement @gautierronan comments
    
    * Correct t_save / dt integer check
    
    My previous solution was not always working. This one is more robust.
    
    ---------
    
    Co-authored-by: Ronan Gautier <ron.gautier@gmail.com>
    
    * @PierreGuilmin's comments and change trace computation
    
    * Add real to btrace
    
    * Change btrace for einsum version
    
    * Implement einsum functions
    
    * Simplify btrace notation
    
    * @PierreGuilmin's comments
    
    * Change trace for normal use
    
    ---------
    
    Co-authored-by: Adrien Bocquet <adrienbocquet38@gmail.com>

--- torchqdynamics/mesolve.py
+++ torchqdynamics/mesolve.py
@@ -1,3 +1,5 @@
+from math import sqrt
+
 import torch
 
 from .odeint import odeint
@@ -8,14 +10,15 @@ def mesolve(
     H, jump_ops, rho0, tsave, solver=None, sensitivity='autograd', parameters=None
 ):
     if solver is None:
-        # TODO: The default dt should not be choosen in such an arbitrary
-        # fashion, which depends on the time unit used by the user.
+        # TODO: Replace by adaptive time step solver when implemented.
         solver = Rouchon(dt=1e-2)
 
     # define the QSolver
     if isinstance(solver, Rouchon):
         if solver.order == 1:
             qsolver = MERouchon1(H, jump_ops, solver)
+        elif solver.order == 1.5:
+            qsolver = MERouchon1_5(H, jump_ops, solver)
         elif solver.order == 2:
             qsolver = MERouchon2(H, jump_ops, solver)
     else:
@@ -49,18 +52,38 @@ class MERouchon(QSolver):
 class MERouchon1(MERouchon):
     def forward(self, t, dt, rho):
         """Compute rho(t+dt) using a Rouchon method of order 1."""
-        # mon-hermitian Hamiltonian at time t
+        # non-hermitian Hamiltonian at time t
         H_nh = self.H(t) - 0.5j * self.sum_nojump
 
-        # build time-dependent Kraus operator
+        # build time-dependent Kraus operators
         M0 = self.I - 1j * dt * H_nh
+        Ms = torch.cat((M0[None, ...], sqrt(dt) * self.jump_ops))
 
         # compute rho(t+dt)
-        rho = (
-            M0 @ rho @ M0.adjoint() + dt *
-            (self.jump_ops @ rho.unsqueeze(0) @ self.jumpdag_ops).sum(dim=0)
-        )
-        rho = rho / rho.trace()
+        rho = kraus_map(rho, Ms)
+        return rho / trace(rho)[..., None, None].real
+
+    def forward_adjoint(self, t, dt, phi):
+        raise NotImplementedError
+
+
+class MERouchon1_5(MERouchon):
+    def forward(self, t, dt, rho):
+        """Compute rho(t+dt) using a Rouchon method of order 1.5."""
+        # non-hermitian Hamiltonian at time t
+        H_nh = self.H(t) - 0.5j * self.sum_nojump
+
+        # build time-dependent Kraus operators
+        M0 = self.I - 1j * dt * H_nh
+        Ms = torch.cat((M0[None, ...], sqrt(dt) * self.jump_ops))
+
+        # build normalization matrix
+        S = M0.adjoint() @ M0 + dt * self.sum_nojump
+        S_inv_sqrtm = inv_sqrtm(S)
+
+        # compute rho(t+dt)
+        rho = kraus_map(rho, S_inv_sqrtm[None, ...])
+        rho = kraus_map(rho, Ms)
         return rho
 
     def forward_adjoint(self, t, dt, phi):
@@ -69,7 +92,57 @@ class MERouchon1(MERouchon):
 
 class MERouchon2(MERouchon):
     def forward(self, t, dt, rho):
-        raise NotImplementedError
+        """Compute rho(t+dt) using a Rouchon method of order 2.
+
+        NOTE: For fast time-varying Hamiltonians, this method is not order 2 because the
+        second-order time derivative term is neglected. This term could be added in the
+        zero-th order Kraus operator if needed, as M0 += -0.5j * dt**2 * \dot{H}.
+        """
+        # non-hermitian Hamiltonian at time t
+        H_nh = self.H(t) - 0.5j * self.sum_nojump
+
+        # build time-dependent Kraus operators
+        M0 = self.I - 1j * dt * H_nh - 0.5 * dt**2 * H_nh @ H_nh
+        M1s = 0.5 * sqrt(dt) * (self.jump_ops @ M0 + M0 @ self.jump_ops)
+
+        # compute rho(t+dt)
+        rho_ = kraus_map(rho, M1s)
+        rho = kraus_map(rho, M0[None, ...]) + rho_ + 0.5 * kraus_map(rho_, M1s)
+        return rho / trace(rho)[..., None, None].real
 
     def forward_adjoint(self, t, dt, phi):
         raise NotImplementedError
+
+
+def kraus_map(rho, operators):
+    """Compute the application of a Kraus map on an input density matrix.
+
+    This is equivalent to `torch.sum(operators @ rho[None,...] @ operators.adjoint(),
+    dim=0)`. The use of einsum yields better performances on large matrices, but may
+    cause a small overhead on smaller matrices (N <~ 50).
+
+    Args:
+        rho: Density matrix of shape (..., n, n).
+        operators: Kraus operators of shape (b, n, n).
+    Returns:
+        Density matrix of shape (..., n, n) with the Kraus map applied.
+    """
+    return torch.einsum('mij,...jk,mkl->...il', operators, rho, operators.adjoint())
+
+
+def trace(rho):
+    """Compute the batched trace of a tensor over its last two dimensions."""
+    return torch.einsum('...ii', rho)
+
+
+def inv_sqrtm(mat):
+    """Compute the inverse square root of a matrix using its eigendecomposition.
+
+    TODO: Replace with Schur decomposition once released by PyTorch.
+    See the feature request at https://github.com/pytorch/pytorch/issues/78809.
+    Alternatively, see
+    https://github.com/pytorch/pytorch/issues/25481#issuecomment-584896176
+    for sqrtm implementation.
+    """
+    vals, vecs = torch.linalg.eigh(mat)
+    return vecs @ torch.linalg.solve(vecs, torch.diag(vals**(-0.5)), left=False)
