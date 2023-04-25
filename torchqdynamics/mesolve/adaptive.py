6edb212d75e7587dcef5e85d9d9e1a340d6939f3
Ronan Gautier <ron.gautier@gmail.com>
2023-04-25 17:07:00 -0400

    Fix merging

--- torchqdynamics/mesolve/adaptive.py
+++ torchqdynamics/mesolve/adaptive.py
@@ -1,14 +1,6 @@
 from torch import Tensor
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 from ..ode.forward_solver import ForwardSolver
-=======
-from ..ode.ode_forward_solver import ODEForwardSolver
->>>>>>> 78bc0c8 (Reorganize main folders)
-=======
-from ..ode.forward_solver import ODEForwardSolver
->>>>>>> 9b673fa (Rename ODE files)
 from ..utils.solver_utils import kraus_map
 
 
