60290510ea62f06024e84ca675a2a72384bdb405
pierreguilmin <pierreguilmin@gmail.com>
2023-05-15 23:23:44 +0200

    Rename dt to delta_t for `Propagator`

--- dynamiqs/sesolve/propagator.py
+++ dynamiqs/sesolve/propagator.py
@@ -5,6 +5,6 @@ from ..solvers.propagator import Propagator
 
 
 class SEPropagator(Propagator):
-    def forward(self, t: float, dt: float, psi: Tensor) -> Tensor:
+    def forward(self, t: float, delta_t: float, psi: Tensor) -> Tensor:
         # psi: (b_H, b_psi, n, 1) -> (b_H, b_psi, n, 1)
-        return torch.matrix_exp(-1j * self.H(t) * dt) @ psi
+        return torch.matrix_exp(-1j * self.H(t) * delta_t) @ psi
