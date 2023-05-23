Fix adjoint gradients from exp_ops

dynamiqs/solvers/ode/fixed_solver.py
@@ -135,7 +135,7 @@ class AdjointFixedAutograd(torch.autograd.Function):
         ctx.save_for_backward(solver.y_save)
 
         # returning `y_save` is required for custom backward functions
-        return solver.y_save
+        return solver.y_save, solver.exp_save
 
     @staticmethod
     def backward(ctx: FunctionCtx, *grad_y: Tensor) -> tuple[None, Tensor, Tensor]:
@@ -164,7 +164,9 @@ class AdjointFixedAutograd(torch.autograd.Function):
         with torch.no_grad():
             # initialize state, adjoint and gradients
             solver.y_bwd = y_save[..., -1, :, :]
-            solver.a_bwd = grad_y[0][..., -1, :, :]
+            solver.a_bwd = (
+                grad_y[0][..., -1, :, :] + grad_y[1][..., :, -1] * solver.exp_ops.mT
+            )
             solver.g_bwd = tuple(
                 torch.zeros_like(_p).to(solver.y_bwd)
                 for _p in solver.options.parameters
@@ -184,7 +186,10 @@ class AdjointFixedAutograd(torch.autograd.Function):
                 solver.y_bwd = y_save[..., i - 1, :, :]
 
                 # update adjoint wrt this time point by adding dL / dy(t)
-                solver.a_bwd += grad_y[0][..., i - 1, :, :]
+                solver.a_bwd += (
+                    grad_y[0][..., i - 1, :, :]
+                    + grad_y[1][..., :, i - 1] * solver.exp_ops.mT
+                )
 
         # convert gradients of real-valued parameters to real-valued gradients
         solver.g_bwd = tuple(