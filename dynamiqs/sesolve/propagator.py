85b86417c7bf21d4bd35555b13f40988357f194b
Ronan Gautier <ron.gautier@gmail.com>
2023-05-26 20:26:38 +0200

    Add caching for SEPropagator

--- dynamiqs/sesolve/propagator.py
+++ dynamiqs/sesolve/propagator.py
@@ -1,3 +1,5 @@
+from functools import lru_cache
+
 import torch
 from torch import Tensor
 
@@ -5,6 +7,11 @@ from ..solvers.propagator import Propagator
 
 
 class SEPropagator(Propagator):
+    @lru_cache(maxsize=1)
+    def propagator(self, t: float, delta_t: float) -> Tensor:
+        # -> (b_H, 1, n, n)
+        return torch.matrix_exp(-1j * self.H(t) * delta_t)
+
     def forward(self, t: float, delta_t: float, psi: Tensor) -> Tensor:
         # psi: (b_H, b_psi, n, 1) -> (b_H, b_psi, n, 1)
-        return torch.matrix_exp(-1j * self.H(t) * delta_t) @ psi
+        return self.propagator(t, delta_t) @ psi
