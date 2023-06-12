29ca61a4a012c6986aab3f666cd6f9934932f052
Pierre Guilmin <pierreguilmin@gmail.com>
2023-06-12 15:26:52 +0200

    Remove fixed solver useless dt argument for forward() and backward() (#136)

--- dynamiqs/sesolve/euler.py
+++ dynamiqs/sesolve/euler.py
@@ -4,6 +4,6 @@ from ..solvers.ode.fixed_solver import FixedSolver
 
 
 class SEEuler(FixedSolver):
-    def forward(self, t: float, dt: float, psi: Tensor) -> Tensor:
+    def forward(self, t: float, psi: Tensor) -> Tensor:
         # psi: (b_H, b_psi, n, 1) -> (b_H, b_psi, n, 1)
-        return psi - dt * 1j * self.H(t) @ psi
+        return psi - self.dt * 1j * self.H(t) @ psi
