121c57342f0d4dc346e2b79f6b71fc2e0c544e51
Ronan Gautier <ron.gautier@gmail.com>
2023-06-20 16:45:19 +0200

    Check propagator Hamiltonian type

--- dynamiqs/solvers/propagator.py
+++ dynamiqs/solvers/propagator.py
@@ -3,10 +3,18 @@ from abc import abstractmethod
 from torch import Tensor
 
 from ..utils.progress_bar import tqdm
+from ..utils.td_tensor import ConstantTDTensor
 from .solver import AutogradSolver
 
 
 class Propagator(AutogradSolver):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # check that Hamiltonian is time-independent
+        if not isinstance(self.H, ConstantTDTensor):
+            raise TypeError('Propagator solvers require time-independent Hamiltonians.')
+
     def run_autograd(self):
         y, t1 = self.y0, 0.0
         for t2 in tqdm(self.t_save.cpu().numpy(), disable=not self.options.verbose):
