0ebb743c3c30b75c13f7b8dd8eb687493e24064a
Ronan Gautier <ron.gautier@gmail.com>
2023-06-20 16:22:21 +0200

    Notation improvements for step

--- dynamiqs/solvers/ode/adaptive_solver.py
+++ dynamiqs/solvers/ode/adaptive_solver.py
@@ -214,21 +214,21 @@ class DormandPrince5(AdaptiveSolver):
         return alpha, beta, csol5, csol5 - csol4
 
     def step(
-        self, f0: Tensor, y0: Tensor, t0: float, dt: float
+        self, f: Tensor, y: Tensor, t: float, dt: float
     ) -> tuple[Tensor, Tensor, Tensor]:
         # import butcher tableau
         alpha, beta, csol, cerr = self.tableau
 
         # compute iterated Runge-Kutta values
-        k = torch.empty(7, *f0.shape, dtype=f0.dtype, device=f0.device)
-        k[0] = f0
+        k = torch.empty(7, *f.shape, dtype=f.dtype, device=f.device)
+        k[0] = f
         for i in range(1, 7):
-            ti = t0 + dt * alpha[i - 1]
-            yi = y0 + torch.tensordot(dt * beta[i - 1, :i], k[:i], dims=([0], [0]))
-            k[i] = self.odefun(ti, yi)
+            dy = torch.tensordot(dt * beta[i - 1, :i], k[:i], dims=([0], [0]))
+            k[i] = self.odefun(t + dt * alpha[i - 1], y + dy)
 
         # compute results
-        y1 = y0 + torch.tensordot(dt * csol[:6], k[:6], dims=([0], [0]))
-        y1_err = torch.tensordot(dt * cerr, k, dims=([0], [0]))
-        f1 = k[-1]
-        return f1, y1, y1_err
+        f_new = k[-1]
+        y_new = y + torch.tensordot(dt * csol[:6], k[:6], dims=([0], [0]))
+        y_err = torch.tensordot(dt * cerr, k, dims=([0], [0]))
+
+        return f_new, y_new, y_err
