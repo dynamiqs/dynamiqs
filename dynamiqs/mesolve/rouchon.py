a94c7c2dcdc95b4ab8bb6360d051acd2524be665
Ronan Gautier <ron.gautier@gmail.com>
2023-07-03 14:08:15 +0200

    Fix import CI

--- dynamiqs/mesolve/rouchon.py
+++ dynamiqs/mesolve/rouchon.py
@@ -6,7 +6,7 @@ import torch
 from torch import Tensor
 
 from ..solvers.ode.fixed_solver import AdjointFixedSolver
-from ..utils.solver_utils import cache, inv_sqrtm, kraus_map
+from ..solvers.utils.utils import cache, inv_sqrtm, kraus_map
 from ..utils.td_tensor import CallableTDTensor, ConstantTDTensor
 from ..utils.utils import unit
 from .me_solver import MESolver
