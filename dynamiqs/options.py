83793f981ae804e220af93dde00cb906988a67ed
pierreguilmin <pierreguilmin@gmail.com>
2023-07-06 23:41:25 +0200

    Fix error message

--- dynamiqs/options.py
+++ dynamiqs/options.py
@@ -51,9 +51,11 @@ class Options:
 
         # check that the gradient algorithm is supported
         if self.gradient_alg is not None and self.gradient_alg not in self.GRADIENT_ALG:
+            available_gradient_alg_str = ', '.join(f'"{x}"' for x in self.GRADIENT_ALG)
             raise ValueError(
-                f'Gradient algorithm {self.gradient_alg} is not defined or not yet'
-                f' supported by solver {type(self)}.'
+                f'Gradient algorithm "{self.gradient_alg}" is not defined or not yet'
+                f' supported by solver {type(self).__name__} (supported:'
+                f' {available_gradient_alg_str}).'
             )
 
     @cached_property
