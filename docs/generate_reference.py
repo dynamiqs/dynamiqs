3c5555dd351e76d62b72eafa6a13bb7b15d12a65
Ronan Gautier <ron.gautier@gmail.com>
2023-07-09 10:24:34 +0200

    Fix generate_reference

--- docs/generate_reference.py
+++ docs/generate_reference.py
@@ -67,13 +67,9 @@ def parse_dunder_all(file_path):
 
 for doc_path, ref_path, src_path in FILES_TO_PARSE:
     # convert various paths
-    doc_path = Path(doc_path)
     ref_path = Path(ref_path)
     src_path = Path(src_path)
     full_ref_path = Path("reference", ref_path.with_suffix(".md"))
-
-    # get global navigation path
-    parts = list(doc_path.parts)
 
     # get global src identifier
     identifier = ".".join(list(src_path.with_suffix("").parts))
@@ -84,7 +80,7 @@ for doc_path, ref_path, src_path in FILES_TO_PARSE:
         full_ref_path_fn = Path("reference", ref_path_fn.with_suffix(".md"))
 
         # add function page to navigation
-        nav[parts + [function]] = ref_path_fn.with_suffix(".md").as_posix()
+        nav[doc_path + [function]] = ref_path_fn.with_suffix(".md").as_posix()
 
         # create the function page
         with mkdocs_gen_files.open(full_ref_path_fn, "w") as fd:
