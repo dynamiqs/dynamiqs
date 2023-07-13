3e3b08e1376953ab9049284d9bf28e17fa26b43d
pierreguilmin <pierreguilmin@gmail.com>
2023-07-13 22:37:45 +0200

    Use pytest parametrize to factor Rouchon tests

--- tests/mesolve/test_rouchon.py
+++ tests/mesolve/test_rouchon.py
@@ -1,3 +1,5 @@
+import pytest
+
 import dynamiqs as dq
 
 from ..solver_tester import SolverTester
@@ -9,37 +11,25 @@ class TestMERouchon1(SolverTester):
         options = dq.options.Rouchon1(dt=1e-2)
         self._test_batching(options, leaky_cavity_8)
 
-    def test_correctness(self):
-        options = dq.options.Rouchon1(dt=1e-3)
+    @pytest.mark.parametrize('sqrt_normalization', [False, True])
+    def test_correctness(self, sqrt_normalization):
+        options = dq.options.Rouchon1(dt=1e-3, sqrt_normalization=sqrt_normalization)
         self._test_correctness(options, leaky_cavity_8, num_t_save=11)
 
-        options = dq.options.Rouchon1(dt=1e-3, sqrt_normalization=True)
-        self._test_correctness(options, leaky_cavity_8, num_t_save=11)
-
-    def test_autograd(self):
-        options = dq.options.Rouchon1(dt=1e-3, gradient_alg='autograd')
-        self._test_gradient(
-            options, grad_leaky_cavity_8, num_t_save=11, rtol=1e-2, atol=1e-2
-        )
-
+    @pytest.mark.parametrize('sqrt_normalization', [False, True])
+    def test_autograd(self, sqrt_normalization):
         options = dq.options.Rouchon1(
-            dt=1e-3, sqrt_normalization=True, gradient_alg='autograd'
+            dt=1e-3, sqrt_normalization=sqrt_normalization, gradient_alg='autograd'
         )
         self._test_gradient(
             options, grad_leaky_cavity_8, num_t_save=11, rtol=1e-2, atol=1e-2
         )
 
-    def test_adjoint(self):
-        options = dq.options.Rouchon1(
-            dt=1e-3, gradient_alg='adjoint', parameters=grad_leaky_cavity_8.parameters
-        )
-        self._test_gradient(
-            options, grad_leaky_cavity_8, num_t_save=11, rtol=5e-2, atol=3e-3
-        )
-
+    @pytest.mark.parametrize('sqrt_normalization', [False, True])
+    def test_adjoint(self, sqrt_normalization):
         options = dq.options.Rouchon1(
             dt=1e-3,
-            sqrt_normalization=True,
+            sqrt_normalization=sqrt_normalization,
             gradient_alg='adjoint',
             parameters=grad_leaky_cavity_8.parameters,
         )
