dcf2a55c34d9afb82f1e0a0f2978bf1909092206
pierreguilmin <pierreguilmin@gmail.com>
2023-09-08 16:00:06 +0200

    Refactor SME Rouchon Solver

--- dynamiqs/smesolve/rouchon.py
+++ dynamiqs/smesolve/rouchon.py
@@ -1,32 +1,45 @@
 from __future__ import annotations
 
+from math import sqrt
+
 import torch
 from torch import Tensor
 
-from ..mesolve.rouchon import MERouchon1
 from ..solvers.ode.fixed_solver import FixedSolver
-from ..solvers.utils import kraus_map
+from ..solvers.utils import cache, kraus_map
 from ..utils.utils import unit
 from .sme_solver import SMESolver
 
 
 class SMERouchon(SMESolver, FixedSolver):
+    pass
+
+
+class SMERouchon1(SMERouchon):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        self.n = self.H.shape[-1]
-        self.I = torch.eye(self.n, device=self.device, dtype=self.cdtype)  # (n, n)
+        # define cached operators
+        # self.M0 (b_H, 1, n, n)
+        self.M0_tmp = cache(lambda Hnh: self.I - 1j * self.dt * Hnh)
 
+        # define M1s
+        # self.M1s: # (1, len(L), n, n)
+        self.M1s = torch.cat(
+            [
+                sqrt(self.dt) * self.T,
+                torch.sqrt(self.dt * (1 - self.etas))[..., None, None] * self.V,
+            ],
+        )[None, ...]
 
-class SMERouchon1(SMERouchon, MERouchon1):
     def forward(self, t: float, rho: Tensor) -> Tensor:
         # rho: (b_H, b_rho, ntrajs, n, n) -> (b_H, b_rho, ntrajs, n, n)
 
         # compute cached operators
-        # H, Hnh, M0: (b_H, 1, ntrajs, n, n)
+        # H, Hnh, M0_tmp: (b_H, 1, ntrajs, n, n)
         H = self.H(t)
         Hnh = self.Hnh(H)
-        M0 = self.M0(Hnh)
+        M0_tmp = self.M0_tmp(Hnh)
 
         # sample Wiener process
         dw = self.sample_wiener(self.dt)
@@ -35,14 +48,9 @@ class SMERouchon1(SMERouchon, MERouchon1):
         dy = self.update_meas(dw, rho)
 
         # update state
-        # M, rho: (b_H, b_rho, ntrajs, n, n)
-        M = M0 + (self.etas.sqrt() * self.V @ dy).sum(-3)
-        rho = (
-            kraus_map(rho, M)
-            + kraus_map(rho, self.M1s)
-            + (1 - self.etas) * kraus_map(rho, self.V) * self.dt
-        )
-
+        # M0: (b_H, 1, ntrajs, n, n)
+        M0 = M0_tmp + ((self.etas.sqrt() * dy)[..., None, None] * self.V).sum(-3)
+        rho = M0 @ rho @ M0.mH + kraus_map(rho, self.M1s)
         rho = unit(rho)
 
         return rho
