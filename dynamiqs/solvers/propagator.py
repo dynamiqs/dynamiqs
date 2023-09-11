fee177d323cff03c9500f606aa95483f987df241
pierreguilmin <pierreguilmin@gmail.com>
2023-09-11 12:25:26 +0200

    Round time difference in propagator solver to prevent useless cache reloading

--- dynamiqs/solvers/propagator.py
+++ dynamiqs/solvers/propagator.py
@@ -1,10 +1,22 @@
+from __future__ import annotations
+
 from abc import abstractmethod
 
+import numpy as np
 from torch import Tensor
 
 from .solver import AutogradSolver
 from .utils.td_tensor import ConstantTDTensor
 from .utils.utils import tqdm
+
+
+def round_truncate(x: np.float32 | np.float64) -> np.float32 | np.float64:
+    # round a strictly positive-valued float to remove numerical errors, and enable
+    # comparing floats for equality
+
+    # round to 5 additional decimal places beyond the highest digit of the number
+    decimals = abs(int(np.log10(x))) + 5
+    return (x * 10**decimals).round() / 10**decimals
 
 
 class Propagator(AutogradSolver):
@@ -21,7 +33,10 @@ class Propagator(AutogradSolver):
     def run_autograd(self):
         y, t1 = self.y0, 0.0
         for t2 in tqdm(self.t_stop.cpu().numpy(), disable=not self.options.verbose):
-            y = self.forward(t1, t2 - t1, y)
+            if t2 != 0.0:
+                # round time difference to avoid numerical errors when comparing floats
+                delta_t = round_truncate(t2 - t1)
+                y = self.forward(t1, delta_t, y)
             self.save(y)
             t1 = t2
 
