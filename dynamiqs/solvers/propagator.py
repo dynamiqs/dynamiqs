28d317ce4b2ffd76ae2908d66e743aad260fbf2c
Pierre Guilmin <pierreguilmin@gmail.com>
2023-09-13 16:21:55 +0200

    Round time difference in propagator solvers to prevent useless cache reloading (#216)
    * Round time difference in propagator solver to prevent useless cache reloading
    
    * Round and truncate the binary version
    
    * Rename internal variable

--- dynamiqs/solvers/propagator.py
+++ dynamiqs/solvers/propagator.py
@@ -1,10 +1,26 @@
+from __future__ import annotations
+
 from abc import abstractmethod
 
+import numpy as np
 from torch import Tensor
 
 from .solver import AutogradSolver
 from .utils.td_tensor import ConstantTDTensor
 from .utils.utils import tqdm
+
+
+def round_truncate(x: np.float32 | np.float64) -> np.float32 | np.float64:
+    # round a strictly positive-valued float to remove numerical errors, and enable
+    # comparing floats for equality
+
+    # The mantissa of a float32 is stored using 23 bits. The following code rounds and
+    # truncates the float value to the 18 most significant bits of its mantissa. This
+    # removes any numerical error that may have accumulated in the 5 least significant
+    # bits of the mantissa.
+    leading = abs(int(np.log2(x)))
+    keep = leading + 18
+    return (x * 2**keep).round() / 2**keep
 
 
 class Propagator(AutogradSolver):
@@ -21,7 +37,10 @@ class Propagator(AutogradSolver):
     def run_autograd(self):
         y, t1 = self.y0, 0.0
         for t2 in tqdm(self.t_stop.cpu().numpy(), disable=not self.options.verbose):
-            y = self.forward(t1, t2 - t1, y)
+            if t2 != 0.0:
+                # round time difference to avoid numerical errors when comparing floats
+                delta_t = round_truncate(t2 - t1)
+                y = self.forward(t1, delta_t, y)
             self.save(y)
             t1 = t2
 
