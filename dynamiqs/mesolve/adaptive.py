80b50485fd9777d58e876ef94b52698747d0fd3f
Ronan Gautier <ron.gautier@gmail.com>
2023-09-17 00:17:19 +0200

    Revert unwanted changes

--- dynamiqs/mesolve/adaptive.py
+++ dynamiqs/mesolve/adaptive.py
@@ -1,5 +1,3 @@
-from __future__ import annotations
-
 from torch import Tensor
 
 from ..solvers.ode.adaptive_solver import AdaptiveSolver, DormandPrince5
@@ -10,13 +8,6 @@ class MEAdaptive(MESolver, AdaptiveSolver):
     def odefun(self, t: float, rho: Tensor) -> Tensor:
         # rho: (b_H, b_rho, n, n) -> (b_H, b_rho, n, n)
         return self.lindbladian(t, rho)
-
-    def odefun_augmented(
-        self, t: float, rho: Tensor, phi: Tensor
-    ) -> tuple[Tensor, Tensor]:
-        # rho: (b_H, b_rho, n, n) -> (b_H, b_rho, n, n)
-        # phi: (b_H, b_rho, n, n) -> (b_H, b_rho, n, n)
-        return -self.lindbladian(t, rho), self.lindbladian_adjoint(t, phi)
 
 
 class MEDormandPrince5(MEAdaptive, DormandPrince5):
