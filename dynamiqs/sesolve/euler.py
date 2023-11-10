7e89cb1d1adeaec2ef4d6b630f7c455425575e1c
pierreguilmin <pierreguilmin@gmail.com>
2023-11-10 21:24:41 +0100

    Reorder SEEuler forward method

--- dynamiqs/sesolve/euler.py
+++ dynamiqs/sesolve/euler.py
@@ -6,4 +6,4 @@ from ..solvers.ode.fixed_solver import FixedSolver
 class SEEuler(FixedSolver):
     def forward(self, t: float, psi: Tensor) -> Tensor:
         # psi: (..., n, 1) -> (..., n, 1)
-        return psi - self.dt * 1j * self.H(t) @ psi
+        return psi - 1j * self.dt * self.H(t) @ psi
