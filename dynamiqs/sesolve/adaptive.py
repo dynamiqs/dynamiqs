45fc7c20fb5bff96158d8457637507d3f0474dbc
pierreguilmin <pierreguilmin@gmail.com>
2023-11-20 14:26:42 +0100

    Revert "Abstract negative time in AdjointAdaptiveSolver"
    This reverts commit 3e4f23efa14592060e4b24268110807e1ade4aa5.

--- dynamiqs/sesolve/adaptive.py
+++ dynamiqs/sesolve/adaptive.py
@@ -13,10 +13,12 @@ class SEAdaptive(AdaptiveSolver):
 
     def odefun_backward(self, t: float, psi: Tensor) -> Tensor:
         # psi: (b_H, 1, b_psi, n, 1) -> (b_H, 1, b_psi, n, 1)
+        # t is passed in as negative time
         raise NotImplementedError
 
     def odefun_adjoint(self, t: float, phi: Tensor) -> Tensor:
         # phi: (b_H, 1, b_psi, n, 1) -> (b_H, 1, b_psi, n, 1)
+        # t is passed in as negative time
         raise NotImplementedError
 
 
