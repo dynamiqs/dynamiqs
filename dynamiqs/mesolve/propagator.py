7716f1a547ece0dbf30f5691b3c026c1069d16bc
pierreguilmin <pierreguilmin@gmail.com>
2023-11-25 14:44:46 +0100

    Generalise solvers shapes comments to arbitrary batching size

--- dynamiqs/mesolve/propagator.py
+++ dynamiqs/mesolve/propagator.py
@@ -10,19 +10,19 @@ from .me_solver import MESolver
 class MEPropagator(MESolver, Propagator):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.lindbladian = slindbladian(self.H, self.L)  # (b_H, b_L, 1, n^2, n^2)
-        self.y0 = operator_to_vector(self.y0)  # (b_H, b_L, b_rho, n^2, 1)
+        self.lindbladian = slindbladian(self.H, self.L)  # (..., n^2, n^2)
+        self.y0 = operator_to_vector(self.y0)  # (..., n^2, 1)
 
     @cache
     def propagator(self, delta_t: float) -> Tensor:
-        # -> (b_H, b_L, 1, n^2, n^2)
+        # -> (..., n^2, n^2)
         return torch.matrix_exp(self.lindbladian * delta_t)
 
     def forward(self, t: float, delta_t: float, rho_vec: Tensor) -> Tensor:
-        # rho: (b_H, b_L, b_rho, n^2, 1) -> (b_H, b_L, b_rho, n^2, 1)
-        return self.propagator(delta_t) @ rho_vec  # (b_H, b_L, b_rho, n^2, 1)
+        # rho: (..., n^2, 1) -> (..., n^2, 1)
+        return self.propagator(delta_t) @ rho_vec  # (..., n^2, 1)
 
     def save(self, y: Tensor):
         # override `save` method to convert `y` from vector to operator
-        y = vector_to_operator(y)  # (b_H, b_L, b_rho, n, n)
+        y = vector_to_operator(y)  # (..., n, n)
         super().save(y)
