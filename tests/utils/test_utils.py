992af97a26da19ce504cc3d826fcc5ef8e2b67b3
Adrien Bocquet <adrienbocquet38@gmail.com>
2023-11-27 09:27:44 +0100

    Add Hadamard operator in utils (#358)
    * Add Hadamard operator in utils
    
    * Update dynamiqs/utils/operators.py
    
    Co-authored-by: Ronan Gautier <ron.gautier@gmail.com>
    
    * Update dynamiqs/utils/operators.py
    
    Co-authored-by: Ronan Gautier <ron.gautier@gmail.com>
    
    * Update dynamiqs/utils/operators.py
    
    Co-authored-by: Ronan Gautier <ron.gautier@gmail.com>
    
    * fix ci
    
    * Add hadamard() to documentation
    
    * Use LaTeX to specify the number of qubits
    
    * Fix matrix newline
    
    * Fix real dtype
    
    * Fix extra newline
    
    * Add higher dimension example
    
    * Simpler expression for H1
    
    * Remove n=0 case
    
    * Replace recursive definition by explicit construction
    
    * Fix test: dtype, factory method for Tensor, explicit atol
    
    ---------
    
    Co-authored-by: Ronan Gautier <ron.gautier@gmail.com>
    Co-authored-by: pierreguilmin <pierreguilmin@gmail.com>

--- tests/utils/test_utils.py
+++ tests/utils/test_utils.py
@@ -1,4 +1,5 @@
 import qutip as qt
+import torch
 from pytest import approx
 
 import dynamiqs as dq
@@ -71,3 +72,39 @@ def test_ket_dm_fidelity_batching():
     rho = dq.to_tensor(rho)
     assert dq.fidelity(rho, psi).shape == (b1, b2)
     assert dq.fidelity(psi, rho).shape == (b1, b2)
+
+
+def test_hadamard():
+    c64 = torch.complex64
+
+    # one qubit
+    H1 = 2 ** (-1 / 2) * torch.tensor([[1, 1], [1, -1]], dtype=c64)
+    assert torch.allclose(dq.hadamard(1), H1)
+
+    # two qubits
+    H2 = 0.5 * torch.tensor(
+        [
+            [1, 1, 1, 1],
+            [1, -1, 1, -1],
+            [1, 1, -1, -1],
+            [1, -1, -1, 1],
+        ],
+        dtype=c64,
+    )
+    assert torch.allclose(dq.hadamard(2), H2)
+
+    # three qubits
+    H3 = 2 ** (-3 / 2) * torch.tensor(
+        [
+            [1, 1, 1, 1, 1, 1, 1, 1],
+            [1, -1, 1, -1, 1, -1, 1, -1],
+            [1, 1, -1, -1, 1, 1, -1, -1],
+            [1, -1, -1, 1, 1, -1, -1, 1],
+            [1, 1, 1, 1, -1, -1, -1, -1],
+            [1, -1, 1, -1, -1, 1, -1, 1],
+            [1, 1, -1, -1, -1, -1, 1, 1],
+            [1, -1, -1, 1, -1, 1, 1, -1],
+        ],
+        dtype=c64,
+    )
+    assert torch.allclose(dq.hadamard(3), H3)
