b9f2ad0cc73e2bec02b76d31a1ea44b709d866b2
baptistehoyau <152294018+baptistehoyau@users.noreply.github.com>
2023-12-08 15:39:18 +0100

    Update dynamiqs/utils/utils.py
    Co-authored-by: Pierre Guilmin <pierreguilmin@gmail.com>

--- dynamiqs/utils/utils.py
+++ dynamiqs/utils/utils.py
@@ -747,7 +747,7 @@ def entropy_vn(x: Tensor) -> Tensor:
 
     Examples:
         >>> rho = dq.unit(dq.fock_dm(2,0) + dq.fock_dm(2,1))
-        >>> entropy_vn(rho)
+        >>> dq.entropy_vn(rho)
         tensor(0.693)
     """
     if isket(x):
