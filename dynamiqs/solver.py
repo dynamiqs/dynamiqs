dbbdf7793e8daa2362f203519f70d882e703281f
Adrien Bocquet <adrien.bocquet@alice-bob.com>
2024-01-05 09:13:44 +0100

    Use solvers configuration

--- dynamiqs/solver.py
+++ dynamiqs/solver.py
@@ -1,4 +1,7 @@
-from .gradient import Gradient
+from jax import Array
+
+from .gradient import Gradient, Autograd, Adjoint
+import diffrax as dx
 
 
 class Solver:
@@ -10,9 +13,58 @@ class Solver:
         return isinstance(gradient, self.SUPPORTED_GRADIENT)
 
 
-class Dopri5(Solver):
+class _ODEFixedStep(Solver):
+    SUPPORTED_GRADIENT = (Autograd,)
+
+    def __init__(self, *, dt: float):
+        # convert `dt` in case a tensor was passed instead of a float
+        if isinstance(dt, Array):
+            dt = dt.item()
+        self.dt = dt
+
+
+class _ODEAdaptiveStep(Solver):
+    SUPPORTED_GRADIENT = (Autograd, Adjoint)
+
+    def __init__(
+        self,
+        *,
+        atol: float = 1e-8,
+        rtol: float = 1e-6,
+        safety_factor: float = 0.9,
+        min_factor: float = 0.2,
+        max_factor: float = 5.0,
+    ):
+        self.atol = atol
+        self.rtol = rtol
+        self.safety_factor = safety_factor
+        self.min_factor = min_factor
+        self.max_factor = max_factor
+
+
+class Dopri5(_ODEAdaptiveStep):
     pass
 
 
-class Euler(Solver):
+class Euler(_ODEFixedStep):
     pass
+
+
+def _stepsize_controller(solver):
+    if isinstance(solver, _ODEFixedStep):
+        stepsize_controller = dx.ConstantStepSize
+        dt = solver.dt
+    elif isinstance(solver, _ODEAdaptiveStep):
+        stepsize_controller = dx.PIDController(
+            rtol=solver.rtol,
+            atol=solver.atol,
+            safety=solver.safety_factor,
+            factormin=solver.min_factor,
+            factormax=solver.max_factor,
+        )
+        dt = None
+    else:
+        raise RuntimeError(
+            'Should never occur, all solvers should be either fixed or adaptive.'
+        )
+    return stepsize_controller, dt
