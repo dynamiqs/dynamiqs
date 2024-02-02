567e77e0052a12c097ada18e2a0524866b16bc0b
pierreguilmin <pierreguilmin@gmail.com>
2024-02-02 13:42:50 +0100

    Adress comments

--- dynamiqs/rand.py
+++ dynamiqs/rand.py
@@ -11,15 +11,11 @@ from .utils.array_types import dtype_complex_to_real, get_cdtype
 
 
 def matrix(
+    key: PRNGKeyArray,
     shape: tuple[int, ...],
     *,
-    key: PRNGKeyArray | None = None,
     dtype: jnp.complex64 | jnp.complex128 | None = None,
 ) -> Array:
-    # seed PRNG key if None
-    if key is None:
-        key = jax.random.PRNGKey(42)
-
     rdtype = dtype_complex_to_real(get_cdtype(dtype))
 
     key1, key2 = jax.random.split(key, 2)
@@ -29,44 +25,44 @@ def matrix(
 
 
 def herm(
+    key: PRNGKeyArray,
     dim: int,
     *,
-    key: PRNGKeyArray | None = None,
     dtype: jnp.complex64 | jnp.complex128 | None = None,
 ) -> Array:
     # hermitian
-    x = matrix((dim, dim), key=key, dtype=dtype)
+    x = matrix(key, (dim, dim), dtype=dtype)
     return 0.5 * (x + dag(x))
 
 
 def psd(
+    key: PRNGKeyArray,
     dim: int,
     *,
-    key: PRNGKeyArray | None = None,
     dtype: jnp.complex64 | jnp.complex128 | None = None,
 ) -> Array:
     # positive semi-definite
-    x = matrix((dim, dim), key=key, dtype=dtype)
+    x = matrix(key, (dim, dim), dtype=dtype)
     return x @ dag(x)
 
 
 def dm(
+    key: PRNGKeyArray,
     dim: int,
     *,
-    key: PRNGKeyArray | None = None,
     dtype: jnp.complex64 | jnp.complex128 | None = None,
 ) -> Array:
-    x = psd(dim, key=key, dtype=dtype)
+    x = psd(key, dim, dtype=dtype)
     x /= x.trace().real
     return x
 
 
 def ket(
+    key: PRNGKeyArray,
     dim: int,
     *,
-    key: PRNGKeyArray | None = None,
     dtype: jnp.complex64 | jnp.complex128 | None = None,
 ) -> Array:
-    x = matrix((dim, 1), key=key, dtype=dtype)
+    x = matrix(key, (dim, 1), dtype=dtype)
     x /= jnp.linalg.norm(x).real
     return x
