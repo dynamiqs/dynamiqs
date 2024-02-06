2c6778f74b6c8b3b1fd71a38fb025566adecf935
Pierre Guilmin <pierreguilmin@gmail.com>
2024-02-06 16:08:27 +0100

    Simplify rand.py and add dtype (#427)
    * Fix rand.py
    
    * Adress comments

--- dynamiqs/rand.py
+++ dynamiqs/rand.py
@@ -1,32 +1,68 @@
+from __future__ import annotations
+
 import jax
-import numpy as np
 from jax import Array
 from jax import numpy as jnp
+from jaxtyping import PRNGKeyArray
 
 from dynamiqs.utils import dag  # todo: clean this dependency
 
+from .utils.array_types import dtype_complex_to_real, get_cdtype
 
-def matrix(dims: tuple[int, ...], key) -> Array:
+
+def matrix(
+    key: PRNGKeyArray,
+    shape: tuple[int, ...],
+    *,
+    dtype: jnp.complex64 | jnp.complex128 | None = None,
+) -> Array:
+    rdtype = dtype_complex_to_real(get_cdtype(dtype))
+
     key1, key2 = jax.random.split(key, 2)
-    mat = jax.random.normal(key1, dims) + 1j * jax.random.normal(key2, dims)
-    return mat
+    x = jax.random.normal(key1, shape, dtype=rdtype)
+    y = jax.random.normal(key2, shape, dtype=rdtype)
+    return x + 1j * y
 
 
-def hermitian(dims: tuple[int, ...], key) -> Array:
-    mat = matrix((np.prod(dims), np.prod(dims)), key=key)
-    return 0.5 * (mat + dag(mat))
+def herm(
+    key: PRNGKeyArray,
+    dim: int,
+    *,
+    dtype: jnp.complex64 | jnp.complex128 | None = None,
+) -> Array:
+    # hermitian
+    x = matrix(key, (dim, dim), dtype=dtype)
+    return 0.5 * (x + dag(x))
 
 
-def dm(dims: tuple[int, ...], key) -> Array:
-    mat = hermitian(dims, key)
-    mat = mat.at[jnp.diag_indices(np.prod(dims))].set(
-        jnp.abs(jnp.diag(mat)) + jnp.sqrt(2) * np.prod(dims)
-    )
-    mat /= jnp.trace(mat)
-    return mat
+def psd(
+    key: PRNGKeyArray,
+    dim: int,
+    *,
+    dtype: jnp.complex64 | jnp.complex128 | None = None,
+) -> Array:
+    # positive semi-definite
+    x = matrix(key, (dim, dim), dtype=dtype)
+    return x @ dag(x)
 
 
-def ket(dims: tuple[int, ...], key) -> Array:
-    vec = matrix((np.prod(dims), 1), key)
-    vec /= jnp.linalg.norm(vec)
-    return vec
+def dm(
+    key: PRNGKeyArray,
+    dim: int,
+    *,
+    dtype: jnp.complex64 | jnp.complex128 | None = None,
+) -> Array:
+    x = psd(key, dim, dtype=dtype)
+    x /= x.trace().real
+    return x
+
+
+def ket(
+    key: PRNGKeyArray,
+    dim: int,
+    *,
+    dtype: jnp.complex64 | jnp.complex128 | None = None,
+) -> Array:
+    x = matrix(key, (dim, 1), dtype=dtype)
+    x /= jnp.linalg.norm(x).real
+    return x
