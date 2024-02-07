37555f919b502716d596cab8e08a2219c008fb7f
pierreguilmin <pierreguilmin@gmail.com>
2024-02-07 14:55:52 +0100

    Add JIT mesolve

--- dynamiqs/__init__.py
+++ dynamiqs/__init__.py
@@ -2,10 +2,10 @@ from importlib.metadata import version
 
 from . import dark, rand
 from ._utils import *  # todo: remove, dev purpose only
-from .mesolve import mesolve
 from .plots import *
 from .result import Result
 from .sesolve import sesolve
+from .mesolve import mesolve
 from .time_array import TimeArray, TimeArrayLike, totime
 from .utils import *
 
