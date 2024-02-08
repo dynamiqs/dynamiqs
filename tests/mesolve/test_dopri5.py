af91eaf6e6c2a616ebc1544c283950bc46f07d52
Ronan Gautier <ron.gautier@gmail.com>
2024-02-08 18:57:53 +0100

    Fix remaining mesolve tests

--- tests/mesolve/test_dopri5.py
+++ tests/mesolve/test_dopri5.py
@@ -4,16 +4,14 @@ from dynamiqs.gradient import Autograd
 from dynamiqs.solver import Dopri5
 
 from ..solver_tester import SolverTester
-from .open_system import ocavity
+from .open_system import ocavity, otdqubit
 
 
 class TestMEDopri5(SolverTester):
-    # @pytest.mark.parametrize('system', [ocavity, otdqubit])
-    @pytest.mark.parametrize('system', [ocavity])
+    @pytest.mark.parametrize('system', [ocavity, otdqubit])
     def test_correctness(self, system):
         self._test_correctness(system, Dopri5())
 
-    # @pytest.mark.parametrize('system', [ocavity, otdqubit])
-    @pytest.mark.parametrize('system', [ocavity])
+    @pytest.mark.parametrize('system', [ocavity, otdqubit])
     def test_autograd(self, system):
         self._test_gradient(system, Dopri5(), Autograd())
