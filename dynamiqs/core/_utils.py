d2ce02dea1f242e01c389108e5f061ee79de4734
Ronan Gautier <ron.gautier@gmail.com>
2024-02-18 12:08:44 +0100

    Add support for arbitrary jump_ops batching

--- dynamiqs/core/_utils.py
+++ dynamiqs/core/_utils.py
@@ -1,7 +1,5 @@
 from __future__ import annotations
 
-from typing import get_args
-
 import jax
 import numpy as np
 from jaxtyping import ArrayLike
@@ -17,8 +15,10 @@ def _astimearray(x: ArrayLike | TimeArray) -> TimeArray:
     else:
         try:
             return _factory_constant(x)
-        except:
-            raise TypeError('Input must be already be a `TimeArray` or an array-like object.')
+        except TypeError:
+            raise TypeError(
+                'Input must be already be a `TimeArray` or an array-like object.'
+            )
 
 
 def get_solver_class(
@@ -36,22 +36,49 @@ def get_solver_class(
 def compute_vmap(
     f: callable,
     cartesian_batching: bool,
-    is_batched: list[bool],
+    is_batched: list[bool | list[bool]],
     out_axes: list[int | None],
 ) -> callable:
-    if any(is_batched):
+    is_batched_flat = flatten(is_batched)
+    if any(is_batched_flat):
         if cartesian_batching:
             # iteratively map over the first axis of each batched argument
-            idx_batched = np.where(is_batched)[0]
+            idx_batched = np.where(is_batched_flat)[0]
             # we apply the successive vmaps in reverse order, so that the output
             # batched dimensions are in the correct order
             for i in reversed(idx_batched):
-                in_axes = [None] * len(is_batched)
+                in_axes = [None] * len(is_batched_flat)
                 in_axes[i] = 0
+                # recover the original structure of the batched arguments
+                in_axes = unflatten(in_axes, is_batched)
+                # apply vmap
                 f = jax.vmap(f, in_axes=in_axes, out_axes=out_axes)
         else:
             # map over the first axis of all batched arguments
-            in_axes = list(np.where(is_batched, 0, None))
+            in_axes = list(np.where(is_batched_flat, 0, None))
+            # recover the original structure of the batched arguments
+            in_axes = unflatten(in_axes, is_batched)
+            # apply vmap
             f = jax.vmap(f, in_axes=in_axes, out_axes=out_axes)
 
     return f
+
+
+def flatten(lst):
+    result = []
+    for item in lst:
+        if isinstance(item, list):
+            result.extend(flatten(item))
+        else:
+            result.append(item)
+    return result
+
+
+def unflatten(flat_list, ref_list):
+    result = []
+    for item in ref_list:
+        if isinstance(item, list):
+            result.append(unflatten(flat_list, item))
+        else:
+            result.append(flat_list.pop(0))
+    return result
--- dynamiqs/mesolve/mesolve.py
+++ dynamiqs/mesolve/mesolve.py
@@ -38,11 +38,18 @@ def mesolve(
     tsave = jnp.asarray(tsave)
     exp_ops = jnp.asarray(exp_ops, dtype=cdtype()) if exp_ops is not None else None
 
+    # === check len(jump_ops) > 0
+    if len(jump_ops) == 0:
+        raise ValueError(
+            "At least one jump operator must be provided for `dq.mesolve`. If you don't"
+            ' need jump operators, consider using `dq.sesolve` instead.'
+        )
+
     # === vectorize function
     # we vectorize over H, jump_ops and rho0, all other arguments are not vectorized
     is_batched = (
         H.ndim > 2,
-        jump_ops_ndim > 3,  # todo: this is a temporary fix
+        [jump_op.ndim > 2 for jump_op in jump_ops],
         rho0.ndim > 2,
         False,
         False,
@@ -56,7 +63,16 @@ def mesolve(
     f = compute_vmap(_mesolve, options.cartesian_batching, is_batched, out_axes)
 
     # === apply vectorized function
-    return f(H, jump_ops, rho0, tsave, exp_ops, solver, gradient, options)
+    return f(
+        H,
+        jump_ops,
+        rho0,
+        tsave,
+        exp_ops,
+        solver,
+        gradient,
+        options,
+    )
 
 
 def _mesolve(
