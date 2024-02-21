c88cb34de73ed6873a1170bd895da5915b452b92
pierreguilmin <pierreguilmin@gmail.com>
2024-02-21 13:53:47 +0100

    Fix TypeError with save_fn function not a valid JAX type

--- dynamiqs/options.py
+++ dynamiqs/options.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 from typing import Callable
 
 import equinox as eqx
+import jax
 from jax import Array
 from jaxtyping import PyTree, Scalar
 
@@ -13,3 +14,8 @@ class Options(eqx.Module):
     cartesian_batching: bool = True
     t0: Scalar | None = None  # defaults to tsave[0]
     save_fn: Callable[[Array], PyTree] | None = None
+
+    def __post_init__(self):
+        if self.save_fn is not None:
+            # use `jax.tree_util.Partial` to make `save_fn` a valid Pytree
+            self.save_fn = jax.tree_util.Partial(self.save_fn)
