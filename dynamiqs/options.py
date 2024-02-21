14508b35ee0ac4458c42b4668f8579339a37f57d
pierreguilmin <pierreguilmin@gmail.com>
2024-02-21 13:18:16 +0100

    Remove documentation for now (see #464)

--- dynamiqs/options.py
+++ dynamiqs/options.py
@@ -8,23 +8,6 @@ from jaxtyping import PyTree, Scalar
 
 
 class Options(eqx.Module):
-    """
-    Options for the solver.
-
-    Parameters:
-        save_states (bool): Whether to save all the states. If `False` only the last
-            state is saved.
-        verbose (bool): Whether to print progress information.
-        cartesian_batching (bool): Whether to perform cartesian product on batched
-            arguments. If `True` the solver will be called with every combination
-            of batched arguments. If `False` the solver will be called with zipped
-            batched arguments.
-        t0 (Scalar, optional): Initial simulation time. Defaults to `tsave[0]`.
-        save_fn (Callable[[Array], PyTree], optional): A function to save
-            additional arbitrary data. The function should take the state
-            as input and return a pytree of arrays. Defaults to `None`.
-    """
-
     save_states: bool = True
     verbose: bool = True
     cartesian_batching: bool = True
