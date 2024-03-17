5bf2ac4e73f8b0066d70c74f85722174dd6dbed0
pierreguilmin <pierreguilmin@gmail.com>
2024-03-17 12:06:38 +1000

    Move documentation to main classs, comment dummy init

--- dynamiqs/options.py
+++ dynamiqs/options.py
@@ -9,11 +9,28 @@ __all__ = ['Options']
 
 
 class Options(eqx.Module):
-    save_states: bool
-    verbose: bool
-    cartesian_batching: bool
-    t0: Scalar | None
-    save_extra: callable[[Array], PyTree] | None
+    """Generic options for the quantum solvers.
+
+    Args:
+        save_states: If `True`, the state is saved at every time in `tsave`,
+            otherwise only the final state is returned.
+        verbose: If `True`, print information about the integration, otherwise
+            nothing is printed.
+        cartesian_batching: If `True`, batched arguments are treated as separated
+            batch dimensions, otherwise the batching is performed over a single
+            shared batched dimension.
+        t0: Initial time. If `None`, defaults to the first time in `tsave`.
+        save_extra _(function, optional)_: A function with signature
+            `f(Array) -> PyTree` that takes a state as input and returns a PyTree.
+            This can be used to save additional arbitrary data during the
+            integration.
+    """
+
+    save_states: bool = True
+    verbose: bool = True
+    cartesian_batching: bool = True
+    t0: Scalar | None = None
+    save_extra: callable[[Array], PyTree] | None = None
 
     def __init__(
         self,
@@ -23,28 +40,12 @@ class Options(eqx.Module):
         t0: Scalar | None = None,
         save_extra: callable[[Array], PyTree] | None = None,
     ):
-        """Generic options for the quantum solvers.
-
-        Args:
-            save_states: If `True`, the state is saved at every time in `tsave`,
-                otherwise only the final state is returned.
-            verbose: If `True`, print information about the integration, otherwise
-                nothing is printed.
-            cartesian_batching: If `True`, batched arguments are treated as separated
-                batch dimensions, otherwise the batching is performed over a single
-                shared batched dimension.
-            t0: Initial time. If `None`, defaults to the first time in `tsave`.
-            save_extra _(function, optional)_: A function with signature
-                `f(Array) -> PyTree` that takes a state as input and returns a PyTree.
-                This can be used to save additional arbitrary data during the
-                integration.
-        """
         self.save_states = save_states
         self.verbose = verbose
         self.cartesian_batching = cartesian_batching
         self.t0 = t0
+
+        # make `save_extra` a valid Pytree with `jax.tree_util.Partial`
         if save_extra is not None:
-            # use `jax.tree_util.Partial` to make `save_extra` a valid Pytree
-            self.save_extra = jax.tree_util.Partial(save_extra)
-        else:
-            self.save_extra = save_extra
+            save_extra = jax.tree_util.Partial(save_extra)
+        self.save_extra = save_extra
