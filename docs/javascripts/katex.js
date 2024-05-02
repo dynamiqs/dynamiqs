2e63198b5d0e97140d7622c3bcdcf928cc9b5bcc
Ronan Gautier <ron.gautier@gmail.com>
2024-05-02 13:02:20 +0200

    Add negative spacing after trace

--- docs/javascripts/katex.js
+++ docs/javascripts/katex.js
@@ -3,7 +3,7 @@ document$.subscribe(({ body }) => {
         "\\dag": '\\dagger',
         "\\dd": '\\mathrm{d}',
         "\\dt": '\\mathrm{d}t',
-        "\\tr": "\\mathrm{Tr}\\left[#1\\right]",
+        "\\tr": "\\mathrm{Tr}\\!\\left[#1\\right]",
         "\\kett": "|#1\\rangle\\!\\rangle",
     };
 
