199e3d89f94794bfdc85ffd4e1af1deda5c596e0
Ronan Gautier <ron.gautier@gmail.com>
2024-05-17 12:19:16 +0200

    Implement dq.fock with many-mode batching

--- dynamiqs/utils/states.py
+++ dynamiqs/utils/states.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 from math import prod
 
 import jax.numpy as jnp
+import numpy as np
 from jax import Array
 from jax.typing import ArrayLike
 
@@ -13,22 +14,27 @@ from .utils import tensor, todm
 __all__ = ['fock', 'fock_dm', 'basis', 'basis_dm', 'coherent', 'coherent_dm']
 
 
-def fock(dim: int | tuple[int, ...], number: int | tuple[int, ...]) -> Array:
+def fock(dim: int | tuple[int, ...], number: ArrayLike) -> Array:
     r"""Returns the ket of a Fock state or the ket of a tensor product of Fock states.
 
     Args:
-        dim _(int or tuple of ints)_: Dimension of the Hilbert space of each mode.
-        number _(int or tuple of ints)_: Fock state number of each mode.
+        dim: Dimension of the Hilbert space of each mode.
+        number _(integer array-like of shape (...) or (..., len(dim)))_): Fock state
+            number of each mode, with last dimension matching the length of `dim` if
+            `dim` is a tuple.
 
     Returns:
-        _(array of shape (n, 1))_ Ket of the Fock state or tensor product of Fock
-            states.
+        _(array of shape (..., prod(dim), 1))_ Ket of the Fock state or
+            tensor product of Fock states.
 
     Examples:
+        Single-mode Fock states:
         >>> dq.fock(3, 1)
         Array([[0.+0.j],
                [1.+0.j],
                [0.+0.j]], dtype=complex64)
+
+        Multi-mode Fock states:
         >>> dq.fock((3, 2), (1, 0))
         Array([[0.+0.j],
                [0.+0.j],
@@ -36,23 +42,92 @@ def fock(dim: int | tuple[int, ...], number: int | tuple[int, ...]) -> Array:
                [0.+0.j],
                [0.+0.j],
                [0.+0.j]], dtype=complex64)
+
+        Batched single-mode Fock states:
+        >>> number = jnp.array([0, 1, 2])
+        >>> dq.fock(3, number)
+        Array([[[1.+0.j],
+                [0.+0.j],
+                [0.+0.j]],
+        <BLANKLINE>
+               [[0.+0.j],
+                [1.+0.j],
+                [0.+0.j]],
+        <BLANKLINE>
+               [[0.+0.j],
+                [0.+0.j],
+                [1.+0.j]]], dtype=complex64)
+
+        Batched multi-mode Fock states:
+        >>> number = [(0, 0), (0, 1), (1, 1), (2, 0)]
+        >>> dq.fock((3, 2), number)
+        Array([[[1.+0.j],
+                [0.+0.j],
+                [0.+0.j],
+                [0.+0.j],
+                [0.+0.j],
+                [0.+0.j]],
+        <BLANKLINE>
+               [[0.+0.j],
+                [1.+0.j],
+                [0.+0.j],
+                [0.+0.j],
+                [0.+0.j],
+                [0.+0.j]],
+        <BLANKLINE>
+               [[0.+0.j],
+                [0.+0.j],
+                [0.+0.j],
+                [1.+0.j],
+                [0.+0.j],
+                [0.+0.j]],
+        <BLANKLINE>
+               [[0.+0.j],
+                [0.+0.j],
+                [0.+0.j],
+                [0.+0.j],
+                [1.+0.j],
+                [0.+0.j]]], dtype=complex64)
     """
-    # convert integer inputs to tuples by default, and check dimensions match
-    dim = (dim,) if isinstance(dim, int) else dim
-    number = (number,) if isinstance(number, int) else number
-    if len(dim) != len(number):
+    # check if dim is an integer or tuple of integers
+    dim = jnp.asarray(dim)
+    dim = dim[None] if dim.ndim == 0 else dim
+    if not jnp.issubdtype(dim.dtype, jnp.integer) and not dim.ndim == 1:
+        raise ValueError('Argument `dim` must be an integer or a tuple of integers.')
+
+    # check if number is an integer array-like object
+    number = jnp.asarray(number)
+    number = number[None] if number.ndim == 0 else number
+    number = number[..., None] if len(dim) == 1 and number.shape[-1] != 1 else number
+    if not jnp.issubdtype(number.dtype, jnp.integer):
+        raise ValueError('Argument `number` must be an integer array-like object.')
+
+    # check number and dim shapes match
+    if dim.shape[-1] != number.shape[-1]:
         raise ValueError(
-            'Arguments `number` must have the same length as `dim` of length'
-            f' {len(dim)}, but has length {len(number)}.'
+            'Arguments `number` and `dim` must have compatible shapes, but got'
+            f' dim.shape={dim.shape}, and number.shape={number.shape}.'
         )
 
-    # compute the required basis state
-    n = 0
-    for d, s in zip(dim, number):
-        n = d * n + s
-    ket = jnp.zeros((prod(dim), 1), dtype=cdtype())
-    ket = ket.at[n].set(1.0)
-    return ket  # noqa: RET504
+    # check if all numbers are within [0, dim)
+    for i, d in enumerate(dim):
+        if jnp.any(jnp.logical_or(number[..., i] < 0, number[..., i] >= d)):
+            raise ValueError(
+                'Fock state number must be in the range [0, dim) for each mode.'
+            )
+
+    # compute all kets
+    number = number.swapaxes(0, -1)  # (len(dim), ...)
+    batch_shape = number.shape[1:]
+    kets = jnp.zeros((*batch_shape, prod(dim), 1), dtype=cdtype())
+    indices = jnp.zeros(batch_shape, dtype=jnp.int32)
+    for d, ns in zip(dim, number):
+        indices = d * indices + ns
+
+    for batch_idx, index in np.ndenumerate(indices):
+        kets = kets.at[(*batch_idx, index, 0)].set(1.0)
+
+    return kets
 
 
 def fock_dm(dim: int | tuple[int, ...], number: int | tuple[int, ...]) -> Array:
