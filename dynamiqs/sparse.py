52d31276a0a84cda6bebcb0bd87e81ae98701ab0
bsaoptima <bogdanstefan.agrici@gmail.com>
2024-05-22 15:18:19 +0200

    add matmul feature to SparseQArray

--- dynamiqs/sparse.py
+++ dynamiqs/sparse.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import equinox as eqx
+import jax
 import jax.numpy as jnp
 from jaxtyping import Array, ArrayLike, ScalarLike
 
@@ -115,6 +116,66 @@ class SparseQArray(QArray):
 
     def __rmul__(self, other: ArrayLike) -> Array:
         return self * other
+
+    def _matmul_dense(self, left_matmul: bool, other: ArrayLike) -> Array:
+        N = other.shape[0]
+        out = jnp.zeros_like(other)
+        for self_offset, self_diag in zip(self.offsets, self.diags):
+            start = max(0, self_offset)
+            end = min(N, N + self_offset)
+            top = max(0, -self_offset)
+            bottom = top + end - start
+            out = jax.lax.cond(
+                left_matmul,
+                lambda _,
+                out=out,
+                top=top,
+                bottom=bottom,
+                self_diag=self_diag,
+                start=start,
+                end=end: out.at[top:bottom, :].add(
+                    self_diag[start:end, None] * other[start:end, :]
+                ),
+                lambda _,
+                out=out,
+                top=top,
+                bottom=bottom,
+                self_diag=self_diag,
+                start=start,
+                end=end: out.at[:, start:end].add(
+                    jnp.expand_dims(self_diag[start:end], axis=0) * other[:, top:bottom]
+                ),
+                operand=None,
+            )
+        return out
+
+    def _matmul_dia(self, other: SparseQArray) -> tuple[Array, list[int]]:
+        N = other.diags.shape[-1]
+        out_diags, out_offsets = [], []
+        for self_offset, self_diag in zip(self.offsets, self.diags):
+            for other_offset, other_diag in zip(other.offsets, other.diags):
+                out_diag = jnp.zeros_like(self_diag)
+                sA, sB = max(0, -other_offset), max(0, other_offset)
+                eA, eB = min(N, N - other_offset), min(N, N + other_offset)
+                out_diag = out_diag.at[sB:eB].add(self_diag[sA:eA] * other_diag[sB:eB])
+                out_diags.append(out_diag)
+                out_offsets.append(self_offset + other_offset)
+        return SparseQArray(jnp.vstack(out_diags), tuple(out_offsets), self.dims)
+
+    def __matmul__(self, other: Array | SparseQArray) -> Array | SparseQArray:
+        if isinstance(other, Array):
+            return self._matmul_dense(left_matmul=True, other=other)
+
+        elif isinstance(other, SparseQArray):
+            return self._matmul_dia(other=other)
+
+        return NotImplemented
+
+    def __rmatmul__(self, other: Array) -> Array:
+        if isinstance(other, Array):
+            return self._matmul_dense(left_matmul=False, other=other)
+
+        return NotImplemented
 
 
 def _check_compatible_dims(dims1: tuple[int, ...], dims2: tuple[int, ...]):
