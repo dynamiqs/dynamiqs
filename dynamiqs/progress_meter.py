a75516d9c72850b3ae514157c4d895c9b315dc9c
Pierre Guilmin <pierreguilmin@gmail.com>
2024-05-24 20:38:26 -0500

    New progress bar format (#589)
    * Add tqdm dependency
    
    * New progress bar format
    
    * Fix percentage padding

--- dynamiqs/progress_meter.py
+++ dynamiqs/progress_meter.py
@@ -2,6 +2,7 @@ from abc import abstractmethod
 
 import diffrax as dx
 import equinox as eqx
+import tqdm
 
 __all__ = [
     'AbstractProgressMeter',
@@ -27,6 +28,15 @@ class TextProgressMeter(AbstractProgressMeter):
         return dx.TextProgressMeter()
 
 
+class _DiffraxTqdmProgressMeter(dx.TqdmProgressMeter):
+    @staticmethod
+    def _init_bar() -> tqdm.tqdm:
+        bar_format = (
+            '|{bar}| {percentage:5.1f}% ◆ total {elapsed} ◆ remaining {remaining}'
+        )
+        return tqdm.tqdm(total=100, unit='%', bar_format=bar_format)
+
+
 class TqdmProgressMeter(AbstractProgressMeter):
     def to_diffrax(self) -> dx.AbstractProgressMeter:
-        return dx.TqdmProgressMeter()
+        return _DiffraxTqdmProgressMeter()
