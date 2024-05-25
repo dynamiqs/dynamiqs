004fd7c86440e4dce3f4e5c520b1da44be102aa1
pierreguilmin <pierreguilmin@gmail.com>
2024-05-25 11:19:21 +1000

    Rename progress bar to progress meter in options

--- dynamiqs/options.py
+++ dynamiqs/options.py
@@ -22,11 +22,12 @@ class Options(eqx.Module):
         cartesian_batching: If `True`, batched arguments are treated as separated
             batch dimensions, otherwise the batching is performed over a single
             shared batched dimension.
-        progress_bar: Progress bar indicating how far the solve has progressed. Defaults
-            to a [tqdm](https://github.com/tqdm/tqdm) progress bar. Pass `None` for no
-            output, see other options in [dynamiqs/progress_meter.py](https://github.com/dynamiqs/dynamiqs/blob/main/dynamiqs/progress_meter.py).
-            If gradients are computed, the progress bar only displays during the forward
-            pass.
+        progress_meter: Progress meter indicating how far the solve has progressed.
+            Defaults to a [tqdm](https://github.com/tqdm/tqdm) progress meter. Pass
+            `None` for no output, see other options in
+            [dynamiqs/progress_meter.py](https://github.com/dynamiqs/dynamiqs/blob/main/dynamiqs/progress_meter.py).
+            If gradients are computed, the progress meter only displays during the
+            forward pass.
         t0: Initial time. If `None`, defaults to the first time in `tsave`.
         save_extra _(function, optional)_: A function with signature
             `f(Array) -> PyTree` that takes a state as input and returns a PyTree.
@@ -37,7 +38,7 @@ class Options(eqx.Module):
     save_states: bool = True
     verbose: bool = True
     cartesian_batching: bool = True
-    progress_bar: AbstractProgressMeter | None = TqdmProgressMeter()
+    progress_meter: AbstractProgressMeter | None = TqdmProgressMeter()
     t0: ScalarLike | None = None
     save_extra: callable[[Array], PyTree] | None = None
 
@@ -46,17 +47,17 @@ class Options(eqx.Module):
         save_states: bool = True,
         verbose: bool = True,
         cartesian_batching: bool = True,
-        progress_bar: AbstractProgressMeter | None = TqdmProgressMeter(),  # noqa: B008
+        progress_meter: AbstractProgressMeter | None = TqdmProgressMeter(),  # noqa: B008
         t0: ScalarLike | None = None,
         save_extra: callable[[Array], PyTree] | None = None,
     ):
-        if progress_bar is None:
-            progress_bar = NoProgressMeter()
+        if progress_meter is None:
+            progress_meter = NoProgressMeter()
 
         self.save_states = save_states
         self.verbose = verbose
         self.cartesian_batching = cartesian_batching
-        self.progress_bar = progress_bar
+        self.progress_meter = progress_meter
         self.t0 = t0
 
         # make `save_extra` a valid Pytree with `Partial`
