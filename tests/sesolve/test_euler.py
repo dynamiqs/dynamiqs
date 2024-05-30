5d89d9c476df11cadf620fc1fb19db6fabaeb221
bsaoptima <bogdanstefan.agrici@gmail.com>
2024-05-30 11:30:32 +0200

    add sesolve euler and adaptive tests

--- tests/sesolve/test_euler.py
+++ tests/sesolve/test_euler.py
@@ -4,16 +4,16 @@ from dynamiqs.gradient import Autograd, CheckpointAutograd
 from dynamiqs.solver import Euler
 
 from ..solver_tester import SolverTester
-from .closed_system import cavity, tdqubit
+from .closed_system import cavity, sparse_cavity, sparse_tdqubit, tdqubit
 
 
 class TestSEEuler(SolverTester):
-    @pytest.mark.parametrize('system', [cavity, tdqubit])
+    @pytest.mark.parametrize('system', [cavity, sparse_cavity, tdqubit, sparse_tdqubit])
     def test_correctness(self, system):
         solver = Euler(dt=1e-4)
         self._test_correctness(system, solver, esave_atol=1e-3)
 
-    @pytest.mark.parametrize('system', [cavity, tdqubit])
+    @pytest.mark.parametrize('system', [cavity, sparse_cavity, tdqubit, sparse_tdqubit])
     @pytest.mark.parametrize('gradient', [Autograd(), CheckpointAutograd()])
     def test_gradient(self, system, gradient):
         solver = Euler(dt=1e-4)
