27bce8ebb9e05b3162d6bc231e2ba7d742a02c05
pierreguilmin <pierreguilmin@gmail.com>
2024-06-19 20:52:32 +1000

    Remove pack_dims from base class

--- dynamiqs/qarrays/qarray.py
+++ dynamiqs/qarrays/qarray.py
@@ -14,16 +14,6 @@ from qutip import Qobj
 __all__ = ['QArray']
 
 
-# a decorator that takes a class method f and returns g(f(x))
-def pack_dims(method: callable) -> callable:
-    """Decorator to return a new QArray with the same dimensions as the original one."""
-
-    def wrapper(self: QArray, *args, **kwargs) -> callable:
-        return self.__class__(method(self, *args, **kwargs), dims=self.dims)
-
-    return wrapper
-
-
 class QArray(eqx.Module):
     r"""Quantum array object. DenseQArray is a wrapper around JAX arrays. It offers
     convenience methods to handle more easily quantum states
