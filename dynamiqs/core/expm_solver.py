allow for Hamiltonian broadcasting, add associated tests fix bug where t_idx gets sent to -1 from 0 reorder propagators to be in line with sesolve behavior when batching over Hamiltonians fix bug H.shape[0] -> H.shape[-1] since the last dimension contains info about the Hilbert space dimension, not necessarily the first dimension (could be a batch dim)

dynamiqs/core/expm_solver.py
@@ -34,7 +34,7 @@ class ExpmSolver(BaseSolver):
         # coincide with the times specified in self.ts. So we need to evaluate the
         # matrix exponential for all such regions
         else:
-            times = jnp.sort(jnp.concatenate((self.H.times, self.ts)))
+            times = jnp.sort(jnp.concatenate((self.H.times, self.ts, jnp.asarray(self.t0).reshape(-1))))
         _t_diffs = jnp.diff(times)
         # for times before t0, don't want to include in the propagator calculation
         t_diffs = jnp.where(times[:-1] < self.t0, 0.0, _t_diffs)
@@ -42,7 +42,7 @@ class ExpmSolver(BaseSolver):
         # to be constant over the region times[-2] to times[-1]
         H_at_ts = jnp.stack([self.H(t) for t in times[:-1]])
         # put the t dimension first, since scan works over the first dimension
-        Ht = jnp.einsum("t,...tij->t...ij", t_diffs, H_at_ts)
+        Ht = jnp.einsum("t,t...ij->t...ij", t_diffs, H_at_ts)
         step_propagators = expm(-1j * Ht)
 
         def _reduce(prev_prop, next_prop):
@@ -51,19 +51,22 @@ class ExpmSolver(BaseSolver):
             total_prop = jnp.einsum("...ij,...jk->...ik", next_prop, prev_prop)
             return total_prop, total_prop
 
+        eye_broadcast = jnp.broadcast_to(eye(self.H.shape[-1]), self.H.shape)
         _, propagators_for_times = jax.lax.scan(
-            _reduce, eye(self.H.shape[0]), step_propagators
+            _reduce, eye_broadcast, step_propagators
         )
 
         # extract the propagators at the correct times
         # the -1 is because the indices of the propagators are defined by t_diffs,
         # not times itself
-        t_idxs = jnp.argmin(jnp.abs(times - self.ts[:, None]), axis=1) - 1
+        t_idxs = jnp.argmin(jnp.abs(times - self.ts[:, None]), axis=1)
+        t_idxs = jnp.where(t_idxs > 0, t_idxs - 1, t_idxs)
         propagators = propagators_for_times[t_idxs]
         # note that we can't take the output of scan as final_prop, because
         # it could correspond to a time window of H.times. However
         # the final element of propagators will correspond to the propagator at the final time
         final_prop = propagators[-1]
+        propagators = jnp.einsum("t...ij->...tij", propagators)
         saved = Saved(propagators, None, None)
         saved = self.collect_saved(saved, final_prop)
         return self.result(saved)

tests/propagator/test_propagator.py
@@ -2,7 +2,7 @@ import jax
 import jax.numpy as jnp
 import pytest
 
-from dynamiqs import Options, propagator, sigmay, pwc, eye
+from dynamiqs import Options, propagator, sigmay, pwc, eye, constant, rand_herm
 from dynamiqs.solver import Tsit5
 
 from ..sesolve.closed_system import cavity, tdqubit
@@ -23,17 +23,18 @@ class TestPropagator(SolverTester):
 
     @pytest.mark.parametrize('save_states', [True, False])
     @pytest.mark.parametrize('solver', [None, Tsit5()])
-    def test_correctness_complex(self, save_states, solver, ysave_atol: float = 1e-4):
-        H = sigmay()
+    @pytest.mark.parametrize('nH', [(), (3,), (3, 4)])
+    def test_correctness_complex(self, nH, save_states, solver, ysave_atol: float = 1e-3):
+        H = constant(rand_herm(jax.random.PRNGKey(42), (*nH, 2, 2)))
         t = 10.0
         tsave = jnp.linspace(0.0, t, 3)
         options = Options(save_states=save_states)
         propresult = propagator(H, tsave, solver=solver, options=options).propagator
         if save_states:
-            Hs = jnp.einsum("ij,t->tij", H, tsave)
+            Hs = jnp.einsum("...ij,t->...tij", H.array, tsave)
             trueresult = jax.scipy.linalg.expm(-1j * Hs)
         else:
-            trueresult = jax.scipy.linalg.expm(-1j * H * t)
+            trueresult = jax.scipy.linalg.expm(-1j * H.array * t)
         errs = jnp.linalg.norm(propresult - trueresult)
         assert jnp.all(errs <= ysave_atol)
 
@@ -44,15 +45,15 @@ class TestPropagator(SolverTester):
         values = [3.0, -2.0]
         array = sigmay()
         H = pwc(times, values, array)
-        tsave = jnp.asarray([0.5, 1.5])
+        tsave = jnp.asarray([0.0, 0.5, 1.5, 2.0])
         options = Options(save_states=save_states)
         propresult = propagator(H, tsave, solver=solver, options=options).propagator
         U0 = eye(H.shape[0])
-        U1 = jax.scipy.linalg.expm(-1j * H.array * 3.0 * (0.5 - 0.0))
-        U2 = jax.scipy.linalg.expm(-1j * H.array * -2.0 * (1.5 - 1.0))
+        U1 = jax.scipy.linalg.expm(-1j * H.array * 3.0 * 0.5)
+        U2 = jax.scipy.linalg.expm(-1j * H.array * -2.0 * 0.5)
         if save_states:
-            trueresult = jnp.stack((U0, U2 @ U1))
+            trueresult = jnp.stack((U0, U1, U2 @ U1 @ U1, U2 @ U2 @ U1 @ U1))
         else:
-            trueresult = U2 @ U1
+            trueresult = U2 @ U2 @ U1 @ U1
         errs = jnp.linalg.norm(propresult - trueresult)
         assert jnp.all(errs <= ysave_atol)