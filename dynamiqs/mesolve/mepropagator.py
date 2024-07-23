c9c926fef9edc8a6ebedbd764e65da6865726dee
pierreguilmin <pierreguilmin@gmail.com>
2024-07-23 17:54:45 +0200

    Fix typing of Array to QArray here and there

--- dynamiqs/mesolve/mepropagator.py
+++ dynamiqs/mesolve/mepropagator.py
@@ -1,8 +1,8 @@
 import jax
-from jax import Array
 from jaxtyping import Scalar
 
 from ..core.propagator_solver import MEPropagatorSolver
+from ..qarrays import QArray
 from ..result import Saved
 from ..utils.vectorization import operator_to_vector, slindbladian, vector_to_operator
 
@@ -10,18 +10,18 @@ from ..utils.vectorization import operator_to_vector, slindbladian, vector_to_op
 class MEPropagator(MEPropagatorSolver):
     # supports only ConstantTimeArray
     # TODO: support PWCTimeArray
-    lindbladian: Array
+    lindbladian: QArray
 
     def __init__(self, *args):
         super().__init__(*args)
         self.lindbladian = slindbladian(self.H, self.Ls)  # (n^2, n^2)
         self.y0 = operator_to_vector(self.y0)  # (n^2, 1)
 
-    def forward(self, delta_t: Scalar, y: Array) -> Array:
+    def forward(self, delta_t: Scalar, y: QArray) -> QArray:
         propagator = jax.scipy.linalg.expm(delta_t * self.lindbladian)
         return propagator @ y
 
-    def save(self, y: Array) -> Saved:
+    def save(self, y: QArray) -> Saved:
         # TODO: implement bexpect for vectorized operators and convert at the end
         #       instead ofat each step
         y = vector_to_operator(y)
--- dynamiqs/sesolve/sepropagator.py
+++ dynamiqs/sesolve/sepropagator.py
@@ -1,14 +1,14 @@
 import jax
-from jax import Array
 from jaxtyping import Scalar
 
 from ..core.propagator_solver import SEPropagatorSolver
+from ..qarrays import QArray
 
 
 class SEPropagator(SEPropagatorSolver):
     # supports only ConstantTimeArray
     # TODO: support PWCTimeArray
 
-    def forward(self, delta_t: Scalar, y: Array) -> Array:
+    def forward(self, delta_t: Scalar, y: QArray) -> QArray:
         propagator = jax.scipy.linalg.expm(-1j * self.H * delta_t)
         return propagator @ y
