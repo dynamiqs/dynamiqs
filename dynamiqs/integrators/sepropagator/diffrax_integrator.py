6bf75f6cfe44514a3ed23ec902782ba751b0daf6
Danny <dkweiss@u.northwestern.edu>
2024-07-24 08:31:40 -0400

    per discussion with Ronan and Pierre, matrix-matrix product is faster than matrix-vector product when dealing with square matrices. Leverage this in logic for sepropagator/diffrax_integrator.py
    since I now call H.in_axes directly, needed to slightly modify the tests for sesolve to make H explicitly a constant Hamiltonian

--- dynamiqs/integrators/sepropagator/diffrax_integrator.py
+++ dynamiqs/integrators/sepropagator/diffrax_integrator.py
@@ -1,49 +1,33 @@
 from __future__ import annotations
 
-import equinox as eqx
-import jax.numpy as jnp
-from jax import Array
 from jaxtyping import PyTree
 
-from ...result import Saved
+from ...result import Saved, SEResult
+from ...time_array import Shape
 from ...utils.operators import eye
-from ..apis.sesolve import sesolve
+from .._utils import _cartesian_vectorize
+from ..apis.sesolve import _sesolve
 from ..core.abstract_integrator import SEPropagatorIntegrator
 
 
 class SEPropagatorDiffraxIntegrator(SEPropagatorIntegrator):
     def run(self) -> PyTree:
-        initial_states = eye(self.H.shape[-1])[..., None]
-        options = eqx.tree_at(
-            lambda x: x.cartesian_batching,
-            self.options,
-            True,
-            is_leaf=lambda x: x is None,
-        )
-        seresult = sesolve(
+        # for technical reasons, matrix-matrix product (for square matrices) is about
+        # 20% faster than matrix-vector product. So rather than directly calling
+        # sesolve, here we call _sesolve instead and specify the initial state as
+        # a square matrix (the identity matrix)
+        initial_states = eye(self.H.shape[-1])
+        n_batch = (self.H.in_axes, Shape(), Shape(), Shape(), Shape(), Shape(), Shape())
+        out_axes = SEResult(False, False, False, False, 0, 0)
+        f = _cartesian_vectorize(_sesolve, n_batch, out_axes)
+        seresult = f(
             self.H,
             initial_states,
             self.ts,
-            solver=self.solver,
-            gradient=self.gradient,
-            options=options,
+            None,
+            self.solver,
+            self.gradient,
+            self.options,
         )
-        saved = self.collect_saved(seresult.states)
+        saved = Saved(seresult.states, None, None)
         return self.result(saved, seresult.infos)
-
-    def collect_saved(self, states: Array) -> Saved:
-        """States is the output of sesolve, which we need to reshape
-        into the propagator.
-        """
-        if self.options.save_states:
-            # indices are ...i, t, j. Want to permute them to
-            # t, j, i such that the t index is first and each
-            # column of the propogator corresponds to each initial state
-            ndim = len(states.shape) - 1
-            perm = [*list(range(ndim - 3)), ndim - 2, ndim - 1, ndim - 3]
-            propagators = jnp.transpose(states[..., 0], perm)
-        else:
-            # otherwise, sesolve has only saved the final states
-            # so we only need to permute the final two axes
-            propagators = states[..., 0].swapaxes(-1, -2)
-        return Saved(propagators, None, None)
