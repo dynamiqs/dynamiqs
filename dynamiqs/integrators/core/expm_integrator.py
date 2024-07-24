392a4436f89de26e88bb4eaf901c4a6226114b8a
Ronan Gautier <ron.gautier@gmail.com>
2024-07-24 17:27:29 +0200

    Refactor ExpmIntegrator.run to account for out of bounds times and structure comments

--- dynamiqs/integrators/core/expm_integrator.py
+++ dynamiqs/integrators/core/expm_integrator.py
@@ -26,20 +26,21 @@ class ExpmIntegrator(BaseIntegrator):
             )
 
     def run(self) -> PyTree:
-        # find times at which the Hamiltonian changes
+        # === find times at which the Hamiltonian changes (between t0 and tfinal)
         t0 = jnp.asarray(self.t0).reshape(-1)
         times = _concatenate_sort(t0, self.H.discontinuity_ts, self.ts)
-        # find the time differences
-        _t_diffs = jnp.diff(times)
-        # for times before t0, don't want to include in the propagator calculation
-        t_diffs = jnp.where(times[:-1] < self.t0, 0.0, _t_diffs)
+        inbounds = (times[:-1] >= t0) & (times[1:] <= self.ts[-1])
+        t_diffs = jnp.where(inbounds, jnp.diff(times), 0.0)
+
+        # === compute the propagators
         # don't need the last time in times since the hamiltonian is guaranteed
         # to be constant over the region times[-2] to times[-1]
-        H_at_ts = jnp.stack([self.H(t) for t in times[:-1]])
-        # put the t dimension first, since scan works over the first dimension
-        Ht = jnp.expand_dims(t_diffs, tuple(range(-H_at_ts.ndim + 1, 0))) * H_at_ts
-        step_propagators = expm(-1j * Ht)
+        Hs = jnp.stack([self.H(t) for t in times[:-1]])
+        dims = tuple(range(-Hs.ndim + 1, 0))
+        t_diffs = jnp.expand_dims(t_diffs, dims)
+        step_propagators = expm(-1j * t_diffs * Hs)
 
+        # === combine propagators to get the propagator at each requested time
         def _reduce(prev_prop: Array, next_prop: Array) -> Array:
             # notice the ordering of prev_prop and next_prop, want
             # next_prop to be to the left of prev_prop
@@ -51,7 +52,7 @@ class ExpmIntegrator(BaseIntegrator):
             _reduce, eye_broadcast, step_propagators
         )
 
-        # extract the propagators at the correct times
+        # === extract the propagators at the correct times
         # the -1 is because the indices of the propagators are defined by t_diffs,
         # not times itself
         t_idxs = jnp.argmin(jnp.abs(times - self.ts[:, None]), axis=1)
@@ -63,6 +64,8 @@ class ExpmIntegrator(BaseIntegrator):
         # at the final time
         final_prop = propagators[-1]
         propagators = jnp.moveaxis(propagators, 0, -3)
+
+        # === save the propagators
         saved = Saved(propagators, None, None)
         saved = self.collect_saved(saved, final_prop)
         return self.result(saved)
