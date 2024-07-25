5da9cf1b9bab10c1614795b984cd4f5b81735899
Pierre Guilmin <pierreguilmin@gmail.com>
2024-07-25 06:20:12 -0500

    Various minor nits (fix some typing and imports) (#640)
    * Fix on_cpu() return type
    
    * Fix Array import in _utils.py
    
    * Add NPY201 ruff rule
    
    See https://docs.astral.sh/ruff/rules/numpy2-deprecation/.
    
    * Fix to_qutip() bug for batched arrays
    
    * Remove repeated dosctring type annotation
    
    * Add missing argument annotation

--- dynamiqs/_utils.py
+++ dynamiqs/_utils.py
@@ -4,7 +4,8 @@ from typing import Any
 
 import equinox as eqx
 import jax.numpy as jnp
-from jaxtyping import Array, PyTree
+from jax import Array
+from jaxtyping import PyTree
 
 
 def type_str(type: Any) -> str:  # noqa: A002
@@ -18,7 +19,7 @@ def obj_type_str(x: Any) -> str:
     return type_str(type(x))
 
 
-def on_cpu(x: Array) -> str:
+def on_cpu(x: Array) -> bool:
     # TODO: this is a temporary solution, it won't work when we have multiple devices
     return x.devices().pop().device_kind == 'cpu'
 
