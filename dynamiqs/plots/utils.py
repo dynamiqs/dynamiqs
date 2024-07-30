4f49f8afc10f9040a2e70baca9b96cb42f0b9f4d
pierreguilmin <pierreguilmin@gmail.com>
2024-07-31 00:30:47 +0200

    Change number of alphas in example to hit exact values

--- dynamiqs/plots/utils.py
+++ dynamiqs/plots/utils.py
@@ -347,7 +347,7 @@ def gifit(
 
         ![plot_cos](/figs_code/plot_cos.gif)
 
-        >>> alphas = jnp.linspace(0.0, 3.0, 50)
+        >>> alphas = jnp.linspace(0.0, 3.0, 51)
         >>> states = dq.coherent(20, alphas)
         >>> filename = 'docs/figs_code/coherent_evolution.gif'
         >>> dq.gifit(dq.plot_fock, filename=filename, display=False)(states)
