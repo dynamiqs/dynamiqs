93d89cf8390644d5f7954f68c7f7f633aebdc1e3
Danny <dkweiss@u.northwestern.edu>
2024-08-27 07:16:23 -0400

    floquet modes are vectors, so need the extra dimension of size 1 on the end
    use my definition of sigmap

--- dynamiqs/integrators/floquet/floquet_integrator.py
+++ dynamiqs/integrators/floquet/floquet_integrator.py
@@ -33,7 +33,9 @@ class FloquetIntegrator(BaseIntegrator):
         # minus sign and divide by T to account for e.g. e^{-iHT}
         quasiens = jnp.angle(-evals) / self.T
         quasiens = jnp.where(quasiens > 0.5 * omega_d, quasiens - omega_d, quasiens)
-        saved = FloquetSaved(evecs, quasiens)
+        # want to save floquet modes with shape ijk where i indexes the modes, j has
+        # dimension of the Hilbert dim and k has dimension one (encoding they are kets)
+        saved = FloquetSaved(evecs.T[..., None], quasiens)
         return self.result(saved, infos=U_result.infos)
 
 
@@ -49,10 +51,14 @@ class FloquetIntegrator_t(FloquetIntegrator):
             gradient=self.gradient,
             options=self.options,
         )
-        # floquet modes are stored as column vectors, so the multiplication
-        # by the phases addresses each column vector individually
-        floquet_modes_t = U_result.propagators @ self.floquet_modes_0
+        # floquet_modes_0 have indices fjd where f labels each mode, and jd are the
+        # components of the mode. Turn it into jf for ease of matmuls
+        f_modes_0 = jnp.squeeze(self.floquet_modes_0, axis=-1).T
+        # has indices tjf where t is time
+        floquet_modes_t = U_result.propagators @ f_modes_0
         quasiens_t = self.quasienergies[None, None] * self.ts[:, None, None]
         floquet_modes_t = floquet_modes_t * jnp.exp(1j * quasiens_t)
-        saved = FloquetSaved(floquet_modes_t, self.quasienergies)
+        # want indices to be in order of tfj
+        floquet_modes_t = jnp.transpose(floquet_modes_t, axes=(0, 2, 1))
+        saved = FloquetSaved(floquet_modes_t[..., None], self.quasienergies)
         return self.result(saved, infos=U_result.infos)
