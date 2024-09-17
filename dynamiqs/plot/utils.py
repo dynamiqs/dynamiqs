6f0b674d13b29d7167179d1fbcc2bd665613fe07
Pierre Guilmin <pierreguilmin@gmail.com>
2024-09-17 12:56:59 -0500

    Change default plots font to sans serif (#708)

--- dynamiqs/plot/utils.py
+++ dynamiqs/plot/utils.py
@@ -232,12 +232,8 @@ def mplstyle(*, usetex: bool = False):
             'lines.linewidth': 2.0,
             # fonts
             'text.usetex': usetex,
-            'text.latex.preamble': r'\usepackage{amsfonts}\usepackage{braket}',
-            'font.family': 'serif',
-            'font.serif': 'Times New Roman',
-            # if usetex=False, matplotlib uses mathtext, for which we choose the STIX
-            # font which is designed to blend well with Times
-            'mathtext.fontset': 'stix',
+            'text.latex.preamble': r'\usepackage{braket}',
+            'font.family': 'sans-serif',
         }
     )
 
