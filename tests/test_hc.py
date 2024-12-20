03ec7779beabfa03df0e8547a166fd0b6d9cfe66
Adrien Bocquet <adrien.bocquet@alice-bob.com>
2024-12-20 09:11:17 +0100

    Add test priority

--- tests/test_hc.py
+++ tests/test_hc.py
@@ -1,6 +1,8 @@
 import jax.numpy as jnp
+import pytest
 
 from dynamiqs import QArray, dense, hc, sigmay
+from tests.order import TEST_INSTANT
 
 
 def assert_equal(x, y):
@@ -11,11 +13,13 @@ def assert_equal(x, y):
     assert jnp.array_equal(x, y)
 
 
+@pytest.mark.run(order=TEST_INSTANT)
 def test_hc_qarray():
     x = sigmay(layout=dense)
     assert_equal(x + hc, x + x.dag())
 
 
+@pytest.mark.run(order=TEST_INSTANT)
 def test_hc_jax():
     x = jnp.array([[1.0, 1.0j], [1.0j, 1.0]])
     assert_equal(x + hc, jnp.array([[2.0, 0.0], [0.0, 2.0]]))
