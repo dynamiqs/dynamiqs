4a620a3e62e3223e2ff1753f7dd46101afdb2303
pierreguilmin <pierreguilmin@gmail.com>
2024-12-26 21:51:18 +0100

    Add support for time-qarray

--- dynamiqs/hermitian_conjugate.py
+++ dynamiqs/hermitian_conjugate.py
@@ -1,20 +1,25 @@
+from __future__ import annotations
+
 from ._utils import obj_type_str
 from .qarrays import QArray
+from .time_qarray import TimeQArray
 
 __all__ = ['hc']
 
 
 class HermitianConjugate:
     def __add__(self, _):  # noqa
-        raise TypeError('The symbol `dq.hq` can only be right-added to a qarray.')
+        raise TypeError(
+            'The symbol `dq.hq` can only be right-added to a qarray or a time-qarray.'
+        )
 
-    def __radd__(self, y: QArray) -> QArray:
-        if isinstance(y, QArray):
+    def __radd__(self, y: QArray | TimeQArray) -> QArray | TimeQArray:
+        if isinstance(y, (QArray, TimeQArray)):
             return y + y.dag()
         else:
             raise TypeError(
-                f'The symbol `dq.hq` can only be right-added to a qarray, but it was'
-                f' added to a {obj_type_str(y)}.'
+                f'The symbol `dq.hq` can only be right-added to a qarray or a '
+                f'time-qarray, but it was added to a {obj_type_str(y)}.'
             )
 
 
