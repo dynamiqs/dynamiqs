4f9dcdbe1c9fe54b3595a669c51afddf7c3fee8a
Pierre Guilmin <pierreguilmin@gmail.com>
2025-02-03 07:38:32 +0100

    Update version `v0.3.0` -> `v0.3.1` (#902)
    Update version v0.3.0 -> v0.3.1

--- dynamiqs/__init__.py
+++ dynamiqs/__init__.py
@@ -8,7 +8,7 @@ from .result import *
 from .time_qarray import *
 from .utils import *
 
-__version__ = '0.3.0'
+__version__ = '0.3.1'
 
 # set default matmul precision to 'highest'
 set_matmul_precision('highest')
