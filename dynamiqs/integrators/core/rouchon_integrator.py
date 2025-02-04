14630f11c9423cac4c90908df6e686a41068212d
pierreguilmin <pierreguilmin@gmail.com>
2025-02-04 18:08:36 +0530

    Add Rouchon1 solver for dsmesolve

--- dynamiqs/integrators/core/rouchon_integrator.py
+++ dynamiqs/integrators/core/rouchon_integrator.py
@@ -9,6 +9,7 @@ import jax.numpy as jnp
 from diffrax._custom_types import RealScalarLike, Y
 from diffrax._local_interpolation import LocalLinearInterpolation
 
+from ...qarrays.qarray import QArray
 from ...utils.general import dag
 from ...utils.operators import eye_like
 from .diffrax_integrator import MESolveDiffraxIntegrator
@@ -53,6 +54,43 @@ class Rouchon1DXSolver(RouchonDXSolver):
         return 1
 
 
+def _cholesky_normalize(M0: QArray, LdL: QArray, dt: float, rho: QArray) -> jax.Array:
+    # To normalize the scheme, we compute S = M0d @ M0 + M1d @ M1 and replace
+    #   M0 by ~M0 = M0 @ S^{-1/2}
+    #   M1 by ~M1 = M1 @ S^{-1/2}
+    # such that
+    #   ~M0d @ ~M0 + ~M1d @ ~M1 = Sd^{-1/2} (M0d @ M0 + M1d @ M1) S^{-1/2}
+    #                           = Sd^{-1/2} S S^{-1/2}
+    #                           = I
+    #
+    # For efficiency, we use a Cholesky decomposition instead of computing
+    # S^{-1/2} explicitly. We write S = T @ Td with T lower triangular, and we
+    # replace
+    #   M0 by ~M0 = M0 @ Td^{-1}
+    #   M1 by ~M1 = M1 @ Td^{-1}
+    # such that
+    #   ~M0d @ ~M0 + ~M1d @ ~M1 = T^{-1} @ (M0d @ M0 + M1d @ M1) @ Td^{-1}
+    #                           = T^{-1} @ T @ Td @ Td^{-1}
+    #                           = I
+    #
+    # In practice we directly replace rho_k by Td^{-1} @ rho_k @ T^{-1/2}
+    # instead of computing all ~Mks.
+
+    # dt may be a traced array, so we need to be careful with the sum
+    S = M0.dag() @ M0 + (LdL * dt if LdL != 0.0 else 0.0)
+
+    T = jnp.linalg.cholesky(S.to_jax())  # T lower triangular
+
+    # we want Td^{-1} @ y0 @ T^{-1}
+    rho = rho.to_jax()
+    # solve Td @ x = rho => x = Td^{-1} @ rho
+    rho = jax.lax.linalg.triangular_solve(
+        T, rho, lower=True, transpose_a=True, conjugate_a=True
+    )
+    # solve x @ T = rho => x = rho @ T^{-1}
+    return jax.lax.linalg.triangular_solve(T, rho, lower=True, left_side=True)
+
+
 class MESolveRouchon1Integrator(MESolveDiffraxIntegrator):
     """Integrator computing the time evolution of the Lindblad master equation using the
     Rouchon 1 solver.
@@ -67,26 +105,7 @@ class MESolveRouchon1Integrator(MESolveDiffraxIntegrator):
             #   M0 = I - (iH + 0.5 Ld @ L) dt
             #   M1 = L sqrt(dt)
             #
-            # To normalize the scheme, we compute S = M0d @ M0 + M1d @ M1 and replace
-            #   M0 by ~M0 = M0 @ S^{-1/2}
-            #   M1 by ~M1 = M1 @ S^{-1/2}
-            # such that
-            #   ~M0d @ ~M0 + ~M1d @ ~M1 = Sd^{-1/2} (M0d @ M0 + M1d @ M1) S^{-1/2}
-            #                           = Sd^{-1/2} S S^{-1/2}
-            #                           = I
-            #
-            # For efficiency, we use a Cholesky decomposition instead of computing
-            # S^{-1/2} explicitly. We write S = T @ Td with T lower triangular, and we
-            # replace
-            #   M0 by ~M0 = M0 @ Td^{-1}
-            #   M1 by ~M1 = M1 @ Td^{-1}
-            # such that
-            #   ~M0d @ ~M0 + ~M1d @ ~M1 = T^{-1} @ (M0d @ M0 + M1d @ M1) @ Td^{-1}
-            #                           = T^{-1} @ T @ Td @ Td^{-1}
-            #                           = I
-            #
-            # In practice we directly replace rho_k by Td^{-1} @ rho_k @ T^{-1/2}
-            # instead of computing all ~Mks.
+            # See comment of `_cholesky_normalize()` for the normalisation.
 
             delta_t = t1 - t0
             rho = y0
@@ -98,21 +117,7 @@ class MESolveRouchon1Integrator(MESolveDiffraxIntegrator):
             Ms = [jnp.sqrt(delta_t) * _L for _L in L]
 
             if self.solver.normalize:
-                # delta_t is a traced array, so we need to be careful with the sum
-                S = M0.dag() @ M0 + (LdL * delta_t if LdL != 0.0 else 0.0)
-
-                T = jnp.linalg.cholesky(S.to_jax())  # T lower triangular
-
-                # we want Td^{-1} @ y0 @ T^{-1}
-                rho = rho.to_jax()
-                # solve Td @ x = rho => x = Td^{-1} @ rho
-                rho = jax.lax.linalg.triangular_solve(
-                    T, rho, lower=True, transpose_a=True, conjugate_a=True
-                )
-                # solve x @ T = rho => x = rho @ T^{-1}
-                rho = jax.lax.linalg.triangular_solve(
-                    T, rho, lower=True, left_side=True
-                )
+                rho = _cholesky_normalize(M0, LdL, delta_t, rho)
 
             return M0 @ rho @ dag(M0) + sum([_M @ rho @ dag(_M) for _M in Ms])
 
