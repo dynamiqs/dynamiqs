7da139810a689d5c1f8543e3708860699ea67ee8
Pierre Guilmin <pierreguilmin@gmail.com>
2025-03-18 11:50:46 +0100

    Replace warnings by errors for empty `jump_ops` in stochastic solvers (#941)

--- dynamiqs/integrators/apis/dssesolve.py
+++ dynamiqs/integrators/apis/dssesolve.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-import logging
 from functools import partial
 
 import jax
@@ -344,7 +343,7 @@ def _check_dssesolve_args(
         check_shape(L, f'jump_ops[{i}]', '(n, n)')
 
     if len(Ls) == 0:
-        logging.warning(
+        raise ValueError(
             'Argument `jump_ops` is an empty list, consider using `dq.sesolve()` to'
             ' solve the Schrödinger equation.'
         )
