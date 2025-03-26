9872348b1e66aae4c752e62209aa6dca08b048ec
Gaspard Beugnot <40466739+gaspardbb@users.noreply.github.com>
2025-03-26 23:00:10 +0100

    Adds forward mode support from diffrax (#945)
    * adds forward mode support from diffrax
    
    * Nit extra newline
    
    * Proposed update to the note explaining when choosing fwd mode
    
    ---------
    
    Co-authored-by: pierreguilmin <pierreguilmin@gmail.com>

--- tests/mesolve/test_euler.py
+++ tests/mesolve/test_euler.py
@@ -1,6 +1,6 @@
 import pytest
 
-from dynamiqs.gradient import Autograd, CheckpointAutograd
+from dynamiqs.gradient import Autograd, CheckpointAutograd, ForwardAutograd
 from dynamiqs.method import Euler
 
 from ..integrator_tester import IntegratorTester
@@ -16,7 +16,9 @@ class TestMESolveEuler(IntegratorTester):
         self._test_correctness(system, method, esave_atol=1e-3)
 
     @pytest.mark.parametrize('system', [dense_ocavity, dia_ocavity, otdqubit])
-    @pytest.mark.parametrize('gradient', [Autograd(), CheckpointAutograd()])
+    @pytest.mark.parametrize(
+        'gradient', [Autograd(), CheckpointAutograd(), ForwardAutograd()]
+    )
     def test_gradient(self, system, gradient):
         method = Euler(dt=1e-4)
         self._test_gradient(system, method, gradient, rtol=1e-2, atol=1e-2)
