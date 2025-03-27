8d006a748f262328cd2cf866bdc9f9eb5d18f2eb
pierreguilmin <pierreguilmin@gmail.com>
2025-03-27 23:03:33 +0100

    Fix unknown variable

--- dynamiqs/_checks.py
+++ dynamiqs/_checks.py
@@ -4,6 +4,7 @@ import equinox as eqx
 import jax.numpy as jnp
 from jax import Array
 
+from .qarrays.layout import dense
 from .qarrays.qarray import QArray
 
 _is_perfect_square = lambda n: int(n**0.5) ** 2 == n
@@ -109,7 +110,7 @@ def check_hermitian(x: QArray, argname: str) -> QArray:
 
 def check_qarray_is_dense(x: QArray, argname: str):
     # to check if the layout of qarray is dense
-    if x.layout != dq.dense:
+    if x.layout != dense:
         raise ValueError(
             f'Argument {argname} must have layout dense but has layout {argname}.layout= {x.layout}.'
         )
