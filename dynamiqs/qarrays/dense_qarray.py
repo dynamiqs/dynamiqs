dynamiqs/dynamiqs

commit feaf312aa78d28678e23d563f72b08e086488c0e

Add qarray matmul priority mechanism (#928)

dynamiqs/qarrays/dense_qarray.py
@@ -25,6 +25,8 @@ class DenseQArray(QArray):
 
     data: Array
 
+    __qarray_matmul_priority__ = 0
+
     def _replace(
         self,
         dims: tuple[int, ...] | None = None,
@@ -169,13 +171,9 @@ class DenseQArray(QArray):
         return self._replace(data=data)
 
     def __matmul__(self, y: QArrayLike) -> QArray | Array:
-        from .sparsedia_qarray import SparseDIAQArray
-
         super().__matmul__(y)
         if isinstance(y, DenseQArray):
             data = self.data @ y.data
-        elif isinstance(y, SparseDIAQArray):
-            return NotImplemented
         elif isqarraylike(y):
             data = self.data @ to_jax(y)
         else:

dynamiqs/qarrays/sparsedia_qarray.py
@@ -44,6 +44,8 @@ class SparseDIAQArray(QArray):
     offsets: tuple[int, ...] = eqx.field(static=True)
     diags: Array = eqx.field(converter=jnp.asarray)
 
+    __qarray_matmul_priority__ = 10
+
     def _replace(
         self,
         dims: tuple[int, ...] | None = None,

dynamiqs/qarrays/qarray.py
@@ -233,6 +233,9 @@ class QArray(eqx.Module):
     # in NumPy it is set to 0.
     __array_priority__ = 200
 
+    # similar behaviour to __array_priority__ but for qarray matmul
+    __qarray_matmul_priority__ = 0
+
     def _replace(
         self,
         dims: tuple[int, ...] | None = None,
@@ -546,10 +549,16 @@ class QArray(eqx.Module):
 
     @abstractmethod
     def __matmul__(self, y: QArrayLike) -> QArray | Array:
+        if (
+            hasattr(y, '__qarray_matmul_priority__')
+            and self.__qarray_matmul_priority__ < y.__qarray_matmul_priority__
+        ):
+            return NotImplemented
+
         if isinstance(y, QArray):
             check_compatible_dims(self.dims, y.dims)
 
-        if is_batched_scalar(y):
+        if is_batched_scalar(y):  # noqa: RET503
             raise TypeError('Attempted matrix product between a scalar and a qarray.')
 
     @abstractmethod

commit 663c8206bd15324933efd82067cda1c071df61af

Fix (again) matrix multiplication dense @ sparse
Bug solved in #924 but re-introduced by #928.

dynamiqs/qarrays/dense_qarray.py
@@ -162,7 +162,10 @@ class DenseQArray(QArray):
         return replace(self, data=data)
 
     def __matmul__(self, y: QArrayLike) -> QArray | Array:
-        super().__matmul__(y)
+        out = super().__matmul__(y)
+        if out is NotImplemented:
+            return NotImplemented
+
         if isinstance(y, DenseQArray):
             data = self.data @ y.data
         elif isqarraylike(y):

dynamiqs/qarrays/sparsedia_qarray.py
@@ -266,7 +266,9 @@ class SparseDIAQArray(QArray):
         return NotImplemented
 
     def __matmul__(self, y: QArrayLike) -> QArray:
-        super().__matmul__(y)
+        out = super().__matmul__(y)
+        if out is NotImplemented:
+            return NotImplemented
 
         if isinstance(y, SparseDIAQArray):
             offsets, diags = matmul_sparsedia_sparsedia(