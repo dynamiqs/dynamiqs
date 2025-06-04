88a10fffbf701494e69f7ddb1167d7119276d844
Ronan Gautier <ron.gautier@gmail.com>
2025-06-04 17:19:25 +0200

    Reintroduce save_extra feature for JumpMonteCarlo method

--- dynamiqs/integrators/core/montecarlo_integrator.py
+++ dynamiqs/integrators/core/montecarlo_integrator.py
@@ -1,15 +1,9 @@
-import equinox as eqx
-from jaxtyping import Array
+import warnings
 
 from ...result import Result, SolveSaved
 from ..apis.jssesolve import _vectorized_jssesolve
 from .abstract_integrator import BaseIntegrator
 from .interfaces import MEInterface, SolveInterface
-
-
-class JumpMonteCarloExtra(eqx.Module):
-    clicktimes: Array
-    final_state_norm: Array
 
 
 class MESolveJumpMonteCarloIntegrator(BaseIntegrator, MEInterface, SolveInterface):
@@ -27,17 +21,26 @@ class MESolveJumpMonteCarloIntegrator(BaseIntegrator, MEInterface, SolveInterfac
             self.method.jsse_options,
         )
 
-        # compute saved with the mean results
-        saved = SolveSaved(
-            ysave=jsse_result.mean_states,
-            Esave=jsse_result.mean_expects,
-            extra=JumpMonteCarloExtra(
-                clicktimes=jsse_result.clicktimes,
-                final_state_norm=jsse_result.final_state_norm,
-            ),
-        )
+        # compute mean states and expectation values
+        ysave = jsse_result.mean_states()
+        Esave = jsse_result.mean_expects()
+
+        # compute save_extra if requested
+        extra = None
+        if self.options.save_extra and self.options.save_states:
+            extra = self.options.save_extra(ysave)
+        elif self.options.save_extra:
+            # this is enforced because `save_extra` is not necessarily a linear
+            # function of the states, and thus save_extra(mean(states)) may not
+            # be equal to mean(save_extra(states)).
+            warnings.warn(
+                '`save_extra` is not supported with the `JumpMonteCarlo` method'
+                'unless `save_states` is also set to True.',
+                stacklevel=2,
+            )
 
         # return
+        saved = SolveSaved(ysave=ysave, extra=extra, Esave=Esave)
         return self.result(saved, infos=None)
 
 
