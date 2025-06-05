d544f90280de27d722e894f5e07fab2a2c48a09b
Ronan Gautier <ron.gautier@gmail.com>
2025-06-05 19:04:37 +0200

    Fix save_extra option

--- dynamiqs/integrators/core/montecarlo_integrator.py
+++ dynamiqs/integrators/core/montecarlo_integrator.py
@@ -20,7 +20,9 @@ class MESolveJumpMonteCarloIntegrator(BaseIntegrator, MEInterface, SolveInterfac
 
     def run(self) -> Result:
         # modify nmaxclick in the options passed to jssesolve
-        jsse_options = replace(self.options, nmaxclick=self.method.jsse_nmaxclick)
+        jsse_options = replace(
+            self.options, nmaxclick=self.method.jsse_nmaxclick, save_extra=None
+        )
 
         keys = jnp.asarray(self.method.keys)
 
@@ -45,8 +47,6 @@ class MESolveJumpMonteCarloIntegrator(BaseIntegrator, MEInterface, SolveInterfac
         extra = None
         if self.options.save_extra:
             if self.options.save_states:
-                # todo: vectorize save_extra (expected signature is
-                # `f(QArray) -> PyTree` with a QArray of shape (n, n))
                 extra = self.options.save_extra(ysave)
             else:
                 # this is enforced because `save_extra` is not necessarily a linear
@@ -54,7 +54,8 @@ class MESolveJumpMonteCarloIntegrator(BaseIntegrator, MEInterface, SolveInterfac
                 # be equal to mean(save_extra(states)).
                 warnings.warn(
                     'The `save_extra` option is not supported with the `JumpMonteCarlo`'
-                    'method unless the `save_states` option is also set to True.',
+                    ' method unless the `save_states` option is also set to True.'
+                    ' Falling back to extra=None.',
                     stacklevel=2,
                 )
 
