f6bcc031d088d364517b4b1262581c783f7f7ec2
Ronan Gautier <ron.gautier@gmail.com>
2025-06-11 14:45:57 +0200

    Add tracing test for states

--- tests/utils/test_states.py
+++ tests/utils/test_states.py
@@ -1,3 +1,5 @@
+import jax
+import jax.numpy as jnp
 import numpy as np
 import pytest
 
@@ -41,3 +43,52 @@ def test_coherent():
     )
     state2 = dq.coherent((n1, n2), (alphas1[None, ...], alphas2[:, None]))
     assert np.allclose(state1, state2)
+
+
+@pytest.mark.run(order=TEST_INSTANT)
+def test_tracing():
+    # prepare random keys and dimensions
+    dim = 4
+    dims = (4, 4)
+    alpha = 0.5
+    alphas = jnp.array([0.5, 1.0, 1.5])
+    nth = 0.1
+    nths = jnp.array([0.1, 0.2, 0.3])
+    num = 1
+    nums = jnp.array([1, 2, 3])
+
+    # check that no error is raised while tracing the functions
+    jax.jit(dq.fock, static_argnums=(0,)).trace(dim, num)
+    jax.jit(dq.fock, static_argnums=(0,)).trace(dim, nums)
+    jax.jit(dq.fock, static_argnums=(0,)).trace(dims, (num, num))
+    jax.jit(dq.fock, static_argnums=(0,)).trace(dims, jnp.stack([nums, nums]).T)
+    jax.jit(dq.fock_dm, static_argnums=(0,)).trace(dim, num)
+    jax.jit(dq.fock_dm, static_argnums=(0,)).trace(dim, nums)
+    jax.jit(dq.fock_dm, static_argnums=(0,)).trace(dims, (num, num))
+    jax.jit(dq.fock_dm, static_argnums=(0,)).trace(dims, jnp.stack([nums, nums]).T)
+    jax.jit(dq.basis, static_argnums=(0,)).trace(dim, num)
+    jax.jit(dq.basis, static_argnums=(0,)).trace(dim, nums)
+    jax.jit(dq.basis, static_argnums=(0,)).trace(dims, (num, num))
+    jax.jit(dq.basis, static_argnums=(0,)).trace(dims, jnp.stack([nums, nums]).T)
+    jax.jit(dq.basis_dm, static_argnums=(0,)).trace(dim, num)
+    jax.jit(dq.basis_dm, static_argnums=(0,)).trace(dim, nums)
+    jax.jit(dq.basis_dm, static_argnums=(0,)).trace(dims, (num, num))
+    jax.jit(dq.basis_dm, static_argnums=(0,)).trace(dims, jnp.stack([nums, nums]).T)
+    jax.jit(dq.coherent, static_argnums=(0,)).trace(dim, alpha)
+    jax.jit(dq.coherent, static_argnums=(0,)).trace(dim, alphas)
+    jax.jit(dq.coherent, static_argnums=(0,)).trace(dims, (alpha, alpha))
+    jax.jit(dq.coherent, static_argnums=(0,)).trace(dims, jnp.stack([alphas, alphas]))
+    jax.jit(dq.coherent_dm, static_argnums=(0,)).trace(dim, alpha)
+    jax.jit(dq.coherent_dm, static_argnums=(0,)).trace(dim, alphas)
+    jax.jit(dq.coherent_dm, static_argnums=(0,)).trace(dims, (alpha, alpha))
+    jax.jit(dq.coherent_dm, static_argnums=(0,)).trace(
+        dims, jnp.stack([alphas, alphas])
+    )
+    jax.jit(dq.thermal_dm, static_argnums=(0,)).trace(dim, nth)
+    jax.jit(dq.thermal_dm, static_argnums=(0,)).trace(dim, nths)
+    jax.jit(dq.thermal_dm, static_argnums=(0,)).trace(dims, (nth, nth))
+    jax.jit(dq.thermal_dm, static_argnums=(0,)).trace(dims, jnp.stack([nths, nths]).T)
+    jax.jit(dq.ground).trace()
+    jax.jit(dq.ground_dm).trace()
+    jax.jit(dq.excited).trace()
+    jax.jit(dq.excited_dm).trace()
