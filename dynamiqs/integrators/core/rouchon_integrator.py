4f39b5f57e72c9cc9c8628a226e4e6545b28e492
Pierre Guilmin <pierreguilmin@gmail.com>
2025-06-11 22:11:37 +0200

    Add `Rouchon2` and `Rouchon3` method for `dq.mesolve()` (#967)
    * Add Rouchon2 and Rouchon3 method for mesolve()
    
    * Fix CI
    
    * Simplify repeated doc
    
    * Add better parameterized tests for Rouchon2 and Rouchon3 (higher dt)
    
    * Fix chosen time for H and Ls in fixed step solver
    
    * Fix CI

--- dynamiqs/integrators/core/rouchon_integrator.py
+++ dynamiqs/integrators/core/rouchon_integrator.py
@@ -3,6 +3,9 @@
 
 from __future__ import annotations
 
+from abc import abstractmethod
+from itertools import product
+
 import diffrax as dx
 import jax
 import jax.numpy as jnp
@@ -10,7 +13,6 @@ from diffrax._custom_types import RealScalarLike, Y
 from diffrax._local_interpolation import LocalLinearInterpolation
 
 from ...qarrays.qarray import QArray
-from ...utils.general import dag
 from ...utils.operators import eye_like
 from .diffrax_integrator import MESolveDiffraxIntegrator
 
@@ -33,6 +35,7 @@ class AbstractRouchonTerm(dx.AbstractTerm):
 
 
 class RouchonDXSolver(dx.AbstractSolver):
+    _order: int
     term_structure = AbstractRouchonTerm
     interpolation_cls = LocalLinearInterpolation
 
@@ -48,42 +51,37 @@ class RouchonDXSolver(dx.AbstractSolver):
     def func(self, terms, t0, y0, args):
         pass
 
-
-class Rouchon1DXSolver(RouchonDXSolver):
     def order(self, terms):
-        return 1
+        return self._order
 
 
-def cholesky_normalize(M0: QArray, LdL: QArray, dt: float, rho: QArray) -> jax.Array:
-    # To normalize the scheme, we compute S = M0d @ M0 + M1d @ M1 and replace
-    #   M0 by ~M0 = M0 @ S^{-1/2}
-    #   M1 by ~M1 = M1 @ S^{-1/2}
+def cholesky_normalize(Ms: list[QArray], rho: QArray) -> jax.Array:
+    # To normalize the scheme, we compute
+    #   S = sum_k Mk^† @ Mk
+    # and replace
+    #   Mk by ~Mk = Mk @ S^{-1/2}
     # such that
-    #   ~M0d @ ~M0 + ~M1d @ ~M1 = Sd^{-1/2} (M0d @ M0 + M1d @ M1) S^{-1/2}
-    #                           = Sd^{-1/2} S S^{-1/2}
-    #                           = I
-    #
-    # For efficiency, we use a Cholesky decomposition instead of computing
-    # S^{-1/2} explicitly. We write S = T @ Td with T lower triangular, and we
-    # replace
-    #   M0 by ~M0 = M0 @ Td^{-1}
-    #   M1 by ~M1 = M1 @ Td^{-1}
+    #   sum_k ~Mk^† @ ~Mk = S^{†(-1/2)} @ (sum_k Mk^† @ Mk) @ S^{-1/2}
+    #                   = S^{†(-1/2)} @ S @ S^{-1/2}
+    #                   = I
+    # To (i) keep sparse matrices and (ii) have a generic implementation that also
+    # works for time-dependent systems, we use a Cholesky decomposition at each step
+    # instead of computing S^{-1/2} explicitly. We write S = T @ T^† with T lower
+    # triangular, and we replace
+    #   Mk by ~Mk = Mk @ S^{-1/2}
     # such that
-    #   ~M0d @ ~M0 + ~M1d @ ~M1 = T^{-1} @ (M0d @ M0 + M1d @ M1) @ Td^{-1}
-    #                           = T^{-1} @ T @ Td @ Td^{-1}
-    #                           = I
-    #
-    # In practice we directly replace rho_k by Td^{-1} @ rho_k @ T^{-1/2}
-    # instead of computing all ~Mks.
+    #   #   sum_k ~Mk^† @ ~Mk = T^{-1} @ (sum_k Mk^† @ Mk) @ T^{†(-1)}
+    #                       = T^{-1} @ T @ T^† @ T^{(-1)}
+    #                       = I
+    # In practice we directly replace rho_k by T^{†(-1)} @ rho_k @ T^{-1} instead of
+    # computing all ~Mks.
 
-    # dt may be a traced array, so we need to be careful with the sum
-    S = M0.dag() @ M0 + (LdL * dt if LdL != 0.0 else 0.0)
-
+    S = sum([M.dag() @ M for M in Ms])
     T = jnp.linalg.cholesky(S.to_jax())  # T lower triangular
 
-    # we want Td^{-1} @ y0 @ T^{-1}
+    # we want T^{†(-1)} @ y0 @ T^{-1}
     rho = rho.to_jax()
-    # solve Td @ x = rho => x = Td^{-1} @ rho
+    # solve T^† @ x = rho => x = T^{†(-1)} @ rho
     rho = jax.lax.linalg.triangular_solve(
         T, rho, lower=True, transpose_a=True, conjugate_a=True
     )
@@ -91,39 +89,122 @@ def cholesky_normalize(M0: QArray, LdL: QArray, dt: float, rho: QArray) -> jax.A
     return jax.lax.linalg.triangular_solve(T, rho, lower=True, left_side=True)
 
 
-class MESolveRouchon1Integrator(MESolveDiffraxIntegrator):
+def _expm_taylor(A: QArray, order: int) -> QArray:
+    I = eye_like(A)
+    out = I
+    powers_of_A = I
+    for i in range(1, order + 1):
+        powers_of_A = A @ powers_of_A
+        out += 1 / jax.scipy.special.factorial(i) * powers_of_A
+
+    return out
+
+
+class MESolveRouchonIntegrator(MESolveDiffraxIntegrator):
     """Integrator computing the time evolution of the Lindblad master equation using the
-    Rouchon 1 method.
+    Rouchon method.
     """
 
     @property
     def terms(self) -> dx.AbstractTerm:
         def kraus_map(t0, t1, y0):  # noqa: ANN202
             # The Rouchon update for a single loss channel is:
-            #   rho_{k+1} = M0 @ rho_k @ M0d + M1 @ rho_k @ M1d
-            # with
-            #   M0 = I - (iH + 0.5 Ld @ L) dt
-            #   M1 = L sqrt(dt)
-            #
+            #   rho_{k+1} = sum_k Mk @ rho_k @ Mk^†
             # See comment of `cholesky_normalize()` for the normalisation.
 
-            delta_t = t1 - t0
             rho = y0
-            L, H = self.L(t0), self.H(t0)
-            I = eye_like(H)
-            LdL = sum([_L.dag() @ _L for _L in L])
-
-            M0 = I - (1j * H + 0.5 * LdL) * delta_t
-            Ms = [jnp.sqrt(delta_t) * _L for _L in L]
+            t = (t0 + t1) / 2
+            dt = t1 - t0
+            Ms = self._kraus_ops(t, dt)
 
             if self.method.normalize:
-                rho = cholesky_normalize(M0, LdL, delta_t, rho)
+                rho = cholesky_normalize(Ms, rho)
 
-            return M0 @ rho @ dag(M0) + sum([_M @ rho @ dag(_M) for _M in Ms])
+            return sum([M @ rho @ M.dag() for M in Ms])
 
         return AbstractRouchonTerm(kraus_map)
 
+    @abstractmethod
+    def _kraus_ops(self, t: float, dt: float) -> list[QArray]:
+        pass
+
+
+class MESolveRouchon1Integrator(MESolveRouchonIntegrator):
+    """Integrator computing the time evolution of the Lindblad master equation using the
+    Rouchon 1 method.
+    """
+
+    def _kraus_ops(self, t: float, dt: float) -> list[QArray]:
+        # M0 = I - (iH + 0.5 sum_k Lk^† @ Lk) dt
+        # Mk = Lk sqrt(dt)
+        L, H = self.L(t), self.H(t)
+        LdL = sum([_L.dag() @ _L for _L in L])
+        G = -1j * H - 0.5 * LdL
+        e1 = (dt * G).expm() if self.method.exact_expm else _expm_taylor(dt * G, 1)
+        return [e1] + [jnp.sqrt(dt) * _L for _L in L]
+
 
 mesolve_rouchon1_integrator_constructor = lambda **kwargs: MESolveRouchon1Integrator(
-    **kwargs, diffrax_solver=Rouchon1DXSolver(), fixed_step=True
+    **kwargs, diffrax_solver=RouchonDXSolver(1), fixed_step=True
+)
+
+
+class MESolveRouchon2Integrator(MESolveRouchonIntegrator):
+    """Integrator computing the time evolution of the Lindblad master equation using the
+    Rouchon 2 method.
+    """
+
+    def _kraus_ops(self, t: float, dt: float) -> list[QArray]:
+        L, H = self.L(t), self.H(t)
+        LdL = sum([_L.dag() @ _L for _L in L])
+        G = -1j * H - 0.5 * LdL
+        e1 = (dt * G).expm() if self.method.exact_expm else _expm_taylor(dt * G, 2)
+
+        return (
+            [e1]
+            + [jnp.sqrt(dt / 2) * e1 @ _L for _L in L]
+            + [jnp.sqrt(dt / 2) * _L @ e1 for _L in L]
+            + [jnp.sqrt(dt**2 / 2) * _L1 @ _L2 for _L1, _L2 in product(L, L)]
+        )
+
+
+mesolve_rouchon2_integrator_constructor = lambda **kwargs: MESolveRouchon2Integrator(
+    **kwargs, diffrax_solver=RouchonDXSolver(2), fixed_step=True
+)
+
+
+class MESolveRouchon3Integrator(MESolveRouchonIntegrator):
+    """Integrator computing the time evolution of the Lindblad master equation using the
+    Rouchon 3 method.
+    """
+
+    def _kraus_ops(self, t: float, dt: float) -> list[QArray]:
+        L, H = self.L(t), self.H(t)
+        LdL = sum([_L.dag() @ _L for _L in L])
+        G = -1j * H - 0.5 * LdL
+        e1o3 = (
+            (dt / 3 * G).expm()
+            if self.method.exact_expm
+            else _expm_taylor(dt / 3 * G, 3)
+        )
+        e2o3 = e1o3 @ e1o3
+        e3o3 = e2o3 @ e1o3
+
+        return (
+            [e3o3]
+            + [jnp.sqrt(3 * dt / 4) * e1o3 @ _L @ e2o3 for _L in L]
+            + [jnp.sqrt(dt / 4) * e3o3 @ _L for _L in L]
+            + [
+                jnp.sqrt(dt**2 / 2) * e1o3 @ _L1 @ e1o3 @ _L2 @ e1o3
+                for _L1, _L2 in product(L, L)
+            ]
+            + [
+                jnp.sqrt(dt**3 / 6) * _L1 @ _L2 @ _L3
+                for _L1, _L2, _L3 in product(L, L, L)
+            ]
+        )
+
+
+mesolve_rouchon3_integrator_constructor = lambda **kwargs: MESolveRouchon3Integrator(
+    **kwargs, diffrax_solver=RouchonDXSolver(3), fixed_step=True
 )
