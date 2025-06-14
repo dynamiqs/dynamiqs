2fdd60edb17e88f82c82725740f8f8e7a8c820d6
pierreguilmin <pierreguilmin@gmail.com>
2025-06-14 12:46:05 +0200

    Simplify test_optimal_control.py

--- tests/utils/test_optimal_control.py
+++ tests/utils/test_optimal_control.py
@@ -1,5 +1,4 @@
 import jax
-import jax.numpy as jnp
 import pytest
 
 import dynamiqs as dq
@@ -10,8 +9,8 @@ from ..order import TEST_INSTANT
 @pytest.mark.run(order=TEST_INSTANT)
 def test_snap_gate():
     # prepare inputs
-    phase = jnp.array([0.5, 1.0, 1.5])
-    phases = jnp.stack([phase, phase + 1.0])
+    phase = [0.5, 1.0, 1.5]
+    phases = [phase, phase, phase]
 
     # check that no error is raised while tracing the function
     jax.jit(dq.snap_gate)(phase)
@@ -23,7 +22,7 @@ def test_cd_gate():
     # prepare inputs
     dim = 4
     alpha = 0.5
-    alphas = jnp.array([0.5, 1.0, 1.5])
+    alphas = [0.5, 1.0, 1.5]
 
     # check that no error is raised while tracing the function
     jax.jit(dq.cd_gate, static_argnums=(0,))(dim, alpha)
