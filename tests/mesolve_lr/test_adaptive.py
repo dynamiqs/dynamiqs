b46c30ef53361721d77514cfc081fb226df625a7
p-pietro <90720953+p-pietro@users.noreply.github.com>
2026-01-08 17:56:09 +0100

    Low rank tests

---
+++ tests/mesolve_lr/test_adaptive.py
@@ -0,0 +1,24 @@
+import pytest
+
+from dynamiqs import Options
+from dynamiqs.gradient import BackwardCheckpointed, Direct, Forward
+from dynamiqs.method import Tsit5
+
+from ..integrator_tester import IntegratorTester
+from ..order import TEST_LONG
+from .open_system import dense_ocavity, dia_ocavity, otdqubit
+
+# we only test Tsit5 to keep the unit test suite fast
+
+
+@pytest.mark.run(order=TEST_LONG)
+class TestMESolveAdaptive(IntegratorTester):
+    @pytest.mark.parametrize('system', [dense_ocavity, dia_ocavity, otdqubit])
+    def test_correctness(self, system):
+        options = Options()
+        self._test_correctness(system, Tsit5(), options=options)
+
+    @pytest.mark.parametrize('system', [dense_ocavity, dia_ocavity, otdqubit])
+    @pytest.mark.parametrize('gradient', [Direct(), BackwardCheckpointed(), Forward()])
+    def test_gradient(self, system, gradient):
+        self._test_gradient(system, Tsit5(), gradient)
---
+++ tests/mesolve_lr/test_batching.py
@@ -0,0 +1,89 @@
+import jax
+import jax.numpy as jnp
+import pytest
+
+import dynamiqs as dq
+dq.set_precision('double') # needed for time dependent test
+jax.config.update("jax_enable_x64", True)
+
+from ..order import TEST_LONG
+
+
+def rand_mesolve_args(n, nH, nLs, npsi0, nEs):
+    nkeys = len(nLs) + 3
+    kH, *kLs, kpsi0, kEs = jax.random.split(jax.random.PRNGKey(42), nkeys)
+    H = dq.random.operator(kH, n, batch=nH)
+    Ls = [dq.random.operator(kL, n, batch=nL) for kL, nL in zip(kLs, nLs, strict=True)]
+    psi0 = dq.random.ket(kpsi0, n, batch=npsi0)
+    Es = dq.random.operator(kEs, n, hermitian=False, batch=nEs)
+    return H, Ls, psi0, Es
+
+
+@pytest.mark.run(order=TEST_LONG)
+@pytest.mark.parametrize('nH', [(), (3,), (3, 4)])
+@pytest.mark.parametrize('npsi0', [(), (5,)])
+@pytest.mark.parametrize('nL1', [(), (7, 8)])
+@pytest.mark.parametrize('nL2', [(), (9,)])
+def test_cartesian_batching(nH, npsi0, nL1, nL2):
+    n = 2
+    nLs = [nL1, nL2]
+    nEs = 10
+    ntsave = 11
+
+    # run mesolve
+    H, Ls, psi0, Es = rand_mesolve_args(n, nH, nLs, npsi0, nEs)
+    tsave = jnp.linspace(0, 0.01, ntsave)
+    result = dq.mesolve_lr(H, Ls, psi0, tsave, M=n, exp_ops=Es)
+
+    # check result shape
+    assert result.states.shape == (*nH, *nL1, *nL2, *npsi0, ntsave, n, n)
+    assert result.expects.shape == (*nH, *nL1, *nL2, *npsi0, nEs, ntsave)
+
+
+# H has fixed shape (3, 4, n, n) for the next test case, we test a broad ensemble of
+# compatible broadcastable shape
+@pytest.mark.run(order=TEST_LONG)
+@pytest.mark.parametrize('nL1', [(), (5, 1, 4)])
+@pytest.mark.parametrize('npsi0', [(), (1,), (4,), (3, 1), (3, 4), (5, 1, 4)])
+def test_flat_batching(nL1, npsi0):
+    n = 2
+    nH = (3, 4)
+    nLs = [nL1, ()]
+    nEs = 6
+    ntsave = 11
+
+    # run mesolve
+    H, Ls, psi0, Es = rand_mesolve_args(n, nH, nLs, npsi0, nEs)
+    tsave = jnp.linspace(0, 0.01, ntsave)
+    options = dq.Options(cartesian_batching=False)
+    result = dq.mesolve_lr(H, Ls, psi0, tsave, M=n, exp_ops=Es, options=options)
+
+    # check result shape
+    broadcast_shape = jnp.broadcast_shapes(nH, nL1, npsi0)
+    assert result.states.shape == (*broadcast_shape, ntsave, n, n)
+    assert result.expects.shape == (*broadcast_shape, nEs, ntsave)
+
+
+@pytest.mark.run(order=TEST_LONG)
+def test_batching_boris():
+    n = 9
+    a = dq.destroy(n)
+
+    # first modulated operator (1, 5, 9, 9)
+    omega1 = jnp.linspace(0, 1, 5)[None, :]
+    f = lambda t: jnp.cos(omega1 * t)
+    H1 = dq.modulated(f, a + a.dag())
+
+    # second modulated operator 2 (7, 1, 9, 9)
+    omega2 = jnp.linspace(0, 1, 7)[:, None]
+    f = lambda t: jnp.cos(omega2 * t)
+    H2 = dq.modulated(f, 1j * a - 1j * a.dag())
+
+    # Hamiltonian
+    H = H1 + H2
+
+    rho0 = dq.fock_dm(n, range(3))  # (3, 9, 9)
+    jump_ops = [a]
+    result = dq.mesolve_lr(H, jump_ops, rho0, [0, 1], M=n//2)
+    assert result.states.shape == (7, 5, 3, 2, 9, 9)
+    assert result.tsave is not None
