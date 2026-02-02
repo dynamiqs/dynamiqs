45880b1b39572cbd531abbd17270a0b1d41c1783
Ronan Gautier <ron.gautier@gmail.com>
2026-02-02 09:58:41 +0100

    Fix python API table by renaming plot files (#1055)

--- docs/generate_python_api.py
+++ docs/generate_python_api.py
@@ -28,15 +28,7 @@ doc_sections = {
     'utils/vectorization': (['dynamiqs/utils/vectorization.py'], 'dq'),
     'utils/optimal_control': (['dynamiqs/utils/optimal_control.py'], 'dq'),
     'random': (['dynamiqs/random/'], 'dq.random'),
-    'plot': (
-        [
-            'dynamiqs/plot/',
-            'dynamiqs/plot/wigner.py',
-            'dynamiqs/plot/fock.py',
-            'dynamiqs/plot/hinton.py',
-        ],
-        'dq.plot',
-    ),
+    'plot': (['dynamiqs/plot/'], 'dq.plot'),
 }
 
 
