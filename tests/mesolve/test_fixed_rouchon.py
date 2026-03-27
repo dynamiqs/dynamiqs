9284728d5e412c1572a77a191430e651c77723df
HHeda <hectorhutin@gmail.com>
2026-03-27 18:54:29 +0100

    Add tests for Rouchon4

--- tests/mesolve/test_fixed_rouchon.py
+++ tests/mesolve/test_fixed_rouchon.py
@@ -1,7 +1,7 @@
 import pytest
 
 from dynamiqs.gradient import BackwardCheckpointed, Direct, Forward
-from dynamiqs.method import Rouchon1, Rouchon2, Rouchon3
+from dynamiqs.method import Rouchon1, Rouchon2, Rouchon3, Rouchon4
 
 from ..integrator_tester import IntegratorTester
 from ..order import TEST_LONG
@@ -15,7 +15,7 @@ from ..systems import dense_ocavity, otdqubit
 @pytest.mark.run(order=TEST_LONG)
 class TestMESolveFixedRouchon(IntegratorTester):
     @pytest.mark.parametrize(
-        ('method_class', 'dt'), [(Rouchon1, 1e-4), (Rouchon2, 1e-3), (Rouchon3, 1e-2)]
+        ('method_class', 'dt'), [(Rouchon1, 1e-4), (Rouchon2, 1e-3), (Rouchon3, 1e-2), (Rouchon4, 1e-2)]
     )
     @pytest.mark.parametrize('system', [dense_ocavity, otdqubit])
     def test_correctness(self, method_class, dt, system):
@@ -23,7 +23,7 @@ class TestMESolveFixedRouchon(IntegratorTester):
         self._test_correctness(system, method)
 
     @pytest.mark.parametrize(
-        ('method_class', 'dt'), [(Rouchon1, 1e-4), (Rouchon2, 1e-3), (Rouchon3, 1e-2)]
+        ('method_class', 'dt'), [(Rouchon1, 1e-4), (Rouchon2, 1e-3), (Rouchon3, 1e-2), (Rouchon4, 1e-2)]
     )
     @pytest.mark.parametrize('system', [dense_ocavity, otdqubit])
     @pytest.mark.parametrize('gradient', [Direct(), BackwardCheckpointed(), Forward()])
