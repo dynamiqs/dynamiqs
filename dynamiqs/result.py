3d0cd00152af6367251693cbb7103eba2480973a
Nicolas Lepage <nlepage@higgs.alice-bob.com>
2026-05-21 16:26:15 +0200

    no solution to the typing problem found so reverted to ignore

--- dynamiqs/result.py
+++ dynamiqs/result.py
@@ -124,7 +124,7 @@ class Result(eqx.Module):
 
     @classmethod
     def out_axes(cls) -> OutAxes:
-        return cast(OutAxes, cls(None, None, None, None, 0, 0))
+        return cls(None, None, None, None, 0, 0)  # ty: ignore[invalid-argument-type]
 
 
 class SolveResult(Result):
@@ -187,7 +187,7 @@ class FloquetResult(Result):
 
     @classmethod
     def out_axes(cls) -> OutAxes:
-        return cast(OutAxes, cls(None, None, None, None, 0, 0, None))
+        return cls(None, None, None, None, 0, 0, None)  # ty: ignore[invalid-argument-type]
 
 
 class SESolveResult(SolveResult):
@@ -227,7 +227,7 @@ class StochasticSolveResult(SolveResult):
 
     @classmethod
     def out_axes(cls) -> OutAxes:
-        return cast(OutAxes, cls(None, None, None, None, 0, 0, 0))
+        return cls(None, None, None, None, 0, 0, 0)  # ty: ignore[invalid-argument-type]
 
     def mean_states(self) -> QArray:
         # todo: document
