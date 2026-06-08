50134c485e04da93b451c9ae96282eff9712b2e3
Nicolas Lepage <nlepage@higgs.alice-bob.com>
2026-06-08 13:34:05 +0200

    removed double MaterializedQArray declaration

--- dynamiqs/qarrays/materialized_qarray.py
+++ dynamiqs/qarrays/materialized_qarray.py
@@ -23,10 +23,6 @@ class MaterializedQArray(QArray):
     vectorized: bool = eqx.field(static=True)
     data: DataArray
 
-class MaterializedQArray(QArray):
-    vectorized: bool = eqx.field(static=True)
-    data: DataArray
-
     def __check_init__(self):
 
         super().__check_init__()
