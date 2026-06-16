5badbebde146fae40891b049f023aaeb17ea1a42
p-pietro <90720953+p-pietro@users.noreply.github.com>
2026-06-16 12:12:03 +0200

    Improve typing and naming

--- dynamiqs/integrators/core/low_rank_integrator.py
+++ dynamiqs/integrators/core/low_rank_integrator.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 
 import warnings
 from functools import partial
-from typing import Any
 
 import diffrax as dx
 import equinox as eqx
@@ -35,60 +34,68 @@ def _qr_cache(A: Array) -> tuple[Array, Array]:
 
 def _cholesky_cache(A: Array) -> Array:
     # Precompute A^+ = (A^H A)^-1 A^H once per vector-field evaluation.
-    A_pinv = jsp_linalg.solve(A.conj().T @ A, A.conj().T, assume_a='pos')
-    return jax.lax.stop_gradient(A_pinv)
+    a_pseudoinverse = jsp_linalg.solve(A.conj().T @ A, A.conj().T, assume_a='pos')
+    return jax.lax.stop_gradient(a_pseudoinverse)
 
 
-def _qr_pinv_apply(cache: tuple[Array, Array], rhs: Array) -> Array:
+def _qr_pseudoinverse_apply(cache: tuple[Array, Array], rhs: Array) -> Array:
     q, r = cache
     return jsp_linalg.solve_triangular(r, q.conj().T @ rhs, lower=False)
 
 
-def _qr_pinv_h_apply(cache: tuple[Array, Array], rhs: Array) -> Array:
+def _qr_pseudoinverse_adjoint_apply(cache: tuple[Array, Array], rhs: Array) -> Array:
     q, r = cache
     # (A^+)^H rhs = Q (R^{-H} rhs), where A = Q R.
     return q @ jsp_linalg.solve_triangular(r, rhs, trans='C', lower=False)
 
 
+# TODO: Extend these cached custom JVP rules to higher-order AD
 @jax.custom_jvp
 def _qr_solve_cached(A: Array, cache: tuple[Array, Array], B: Array) -> Array:
     del A
-    return _qr_pinv_apply(cache, B)
+    return _qr_pseudoinverse_apply(cache, B)
 
 
 @_qr_solve_cached.defjvp
 def _qr_solve_cached_jvp(
-    primals: tuple[Any, ...], tangents: tuple[Any, ...]
+    primals: tuple[Array, tuple[Array, Array], Array],
+    tangents: tuple[Array, tuple[Array, Array], Array],
 ) -> tuple[Array, Array]:
     A, cache, B = primals
-    dA, _dcache, dB = tangents
-    x = _qr_solve_cached(A, cache, B)
+    dA, _cache_tangent, dB = tangents
+    solution = _qr_solve_cached(A, cache, B)
     # Pseudoinverse JVP formula (independent columns):
     # dX = A^+ (dB - dA X + (A^+)^H (dA^H (B - A X))).
-    res = B - A @ x
-    dvec = dB - dA @ x + _qr_pinv_h_apply(cache, dA.conj().T @ res)
-    dx = _qr_pinv_apply(cache, dvec)
-    return x, dx
+    residual = B - A @ solution
+    tangent_rhs = (
+        dB
+        - dA @ solution
+        + _qr_pseudoinverse_adjoint_apply(cache, dA.conj().T @ residual)
+    )
+    solution_tangent = _qr_pseudoinverse_apply(cache, tangent_rhs)
+    return solution, solution_tangent
 
 
 @jax.custom_jvp
-def _cholesky_solve_cached(A: Array, A_pinv: Array, B: Array) -> Array:
+def _cholesky_solve_cached(A: Array, a_pseudoinverse: Array, B: Array) -> Array:
     del A
-    return A_pinv @ B
+    return a_pseudoinverse @ B
 
 
 @_cholesky_solve_cached.defjvp
 def _cholesky_solve_cached_jvp(
-    primals: tuple[Any, ...], tangents: tuple[Any, ...]
+    primals: tuple[Array, Array, Array], tangents: tuple[Array, Array, Array]
 ) -> tuple[Array, Array]:
-    A, A_pinv, B = primals
-    dA, _dA_pinv, dB = tangents
-    x = _cholesky_solve_cached(A, A_pinv, B)
+    A, a_pseudoinverse, B = primals
+    dA, _pseudoinverse_tangent, dB = tangents
+    solution = _cholesky_solve_cached(A, a_pseudoinverse, B)
     # Same pseudoinverse formula as the QR path, but using cached A^+.
-    res = B - A @ x
-    dvec = dB - dA @ x + A_pinv.conj().T @ (dA.conj().T @ res)
-    dx = A_pinv @ dvec
-    return x, dx
+    residual = B - A @ solution
+    tangent_rhs = (
+        dB - dA @ solution + a_pseudoinverse.conj().T @ (dA.conj().T @ residual)
+    )
+    solution_tangent = a_pseudoinverse @ tangent_rhs
+    return solution, solution_tangent
 
 
 def normalize_m(m: Array) -> Array:
@@ -253,9 +260,10 @@ class MESolveLowRankIntegrator(
             solve_cache = solver_cache_builder(m)
 
             for L in Ls:
-                Lm = (L @ m).to_jax()
-                tmp = linsolve(m, solve_cache, Lm)
-                dm += 0.5 * (Lm @ tmp.conj().T) - 0.5 * (L.dag() @ Lm).to_jax()
+                jump_action = (L @ m).to_jax()
+                projected_jump_action = linsolve(m, solve_cache, jump_action)
+                dm += 0.5 * (jump_action @ projected_jump_action.conj().T)
+                dm -= 0.5 * (L.dag() @ jump_action).to_jax()
 
             return dm
 
